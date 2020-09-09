046d50345ce40f3f6714f085512cd167198d5103
Rachele Edwards <dot.com1472@gmail.com>
2020-09-09 15:51:34 -0600

    added dummy data of cities to test and implement search bar

---
+++ src/components/pages/Nav/cityList.js
@@ -0,0 +1,33 @@
+// A list of dummy data to get the search form functional
+// Data pulled from: https://simple.wikipedia.org/wiki/List_of_United_States_cities_by_population#Cities_that_used_to_have_100,000_people
+
+export const cities = [
+  "Albany NY",
+  "Allegheny PA",
+  "Brooklyn NY",
+  "Camden NJ",
+  "Canton OH",
+  "Dearborn MI",
+  "Duluth MN",
+  "Erie PA",
+  "Fall River MA",
+  "Flint MI",
+  "Gary IN",
+  "Hammond IN",
+  "Kenosha WI",
+  "Livonia MI",
+  "Lynn MA",
+  "New Bedford MA",
+  "Niagara Falls NY",
+  "Parma OH",
+  "Portsmouth VI",
+  "Reading PA",
+  "Roanoke VA",
+  "Scranton PA",
+  "Somerville MA",
+  "St. Joseph MO",
+  "Trenton NJ",
+  "Utica NY",
+  "Wilmington DW",
+  "Youngstown OH"
+];

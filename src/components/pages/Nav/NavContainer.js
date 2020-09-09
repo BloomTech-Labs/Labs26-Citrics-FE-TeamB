2cff0dd1cd7731e16bb12a2dac56ff2b89bf1eb8
Rachele Edwards <dot.com1472@gmail.com>
2020-09-09 14:14:18 -0600

    added drawer from antd--changing format from class component to funciton

--- src/components/pages/Nav/NavContainer.js
+++ src/components/pages/Nav/NavContainer.js
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Drawer, Button, Radio, Space } from "antd";
 
 export default function NavContainer(props) {
   //import antdesign for drawer
   //implement drawer for navbar feature
   //create search bar form
-  return <div />;
+
+  // state = { visible: false, placement: 'left' };
+  const [state, setState] = useState({
+    visible: false,
+    placement: "left"
+  });
+
+  const showDrawer = () => {
+    setState({
+      visible: true
+    });
+  };
+
+  const onClose = () => {
+    setState({
+      visible: false
+    });
+  };
+
+  const onChange = e => {
+    setState({
+      placement: e.target.value
+    });
+  };
+
+  const { placement, visible } = state;
+  return (
+    <>
+      <Space>
+        <Radio.Group defaultValue={placement} onChange={onChange}>
+          <Radio value="top">top</Radio>
+          <Radio value="right">right</Radio>
+          <Radio value="bottom">bottom</Radio>
+          <Radio value="left">left</Radio>
+        </Radio.Group>
+        <Button type="primary" onClick={showDrawer}>
+          Open
+        </Button>
+      </Space>
+      <Drawer
+        title="Basic Drawer"
+        placement={placement}
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+        key={placement}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
+  );
 }

20336ef6f38536232fc0ee03cb7691b39b82b8b5
Rachele Edwards <dot.com1472@gmail.com>
2020-09-09 15:13:56 -0600

    Refactored drawer and placement so it pops up on the left instead of the right side

--- src/components/pages/Nav/NavContainer.js
+++ src/components/pages/Nav/NavContainer.js
@@ -1,56 +1,32 @@
 import React, { useState } from "react";
-import { Drawer, Button, Radio, Space } from "antd";
+import { Drawer, Button } from "antd";
 
 export default function NavContainer(props) {
   //import antdesign for drawer
   //implement drawer for navbar feature
   //create search bar form
 
-  // state = { visible: false, placement: 'left' };
-  const [state, setState] = useState({
-    visible: false,
-    placement: "left"
-  });
+  const [visible, setVisible] = useState(false);
 
   const showDrawer = () => {
-    setState({
-      visible: true
-    });
+    setVisible(true);
   };
 
   const onClose = () => {
-    setState({
-      visible: false
-    });
+    setVisible(false);
   };
 
-  const onChange = e => {
-    setState({
-      placement: e.target.value
-    });
-  };
-
-  const { placement, visible } = state;
   return (
     <>
-      <Space>
-        <Radio.Group defaultValue={placement} onChange={onChange}>
-          <Radio value="top">top</Radio>
-          <Radio value="right">right</Radio>
-          <Radio value="bottom">bottom</Radio>
-          <Radio value="left">left</Radio>
-        </Radio.Group>
-        <Button type="primary" onClick={showDrawer}>
-          Open
-        </Button>
-      </Space>
+      <Button type="primary" onClick={showDrawer}>
+        Open
+      </Button>
       <Drawer
         title="Basic Drawer"
-        placement={placement}
+        placement="left"
         closable={false}
         onClose={onClose}
         visible={visible}
-        key={placement}
       >
         <p>Some contents...</p>
         <p>Some contents...</p>

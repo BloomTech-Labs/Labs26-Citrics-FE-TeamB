14ebd10542ed305c94f848acf62ee4a05f2ee047
Rachele Edwards <dot.com1472@gmail.com>
2020-09-09 14:56:02 -0600

    removed all Okta/Auth routes to render the drawer and simple div--Sucessful render and funciton of drawer

--- src/components/pages/Home/HomeContainer.js
+++ src/components/pages/Home/HomeContainer.js
@@ -1,41 +1,43 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { useOktaAuth } from '@okta/okta-react';
+import React, { useState, useEffect, useMemo } from "react";
 
-import RenderHomePage from './RenderHomePage';
+// import { useOktaAuth } from '@okta/okta-react';
+
+import RenderHomePage from "./RenderHomePage";
 
 function HomeContainer({ LoadingComponent }) {
-  const { authState, authService } = useOktaAuth();
+  // const { authState, authService } = useOktaAuth();
   const [userInfo, setUserInfo] = useState(null);
   // eslint-disable-next-line
-  const [memoAuthService] = useMemo(() => [authService], []);
+  // const [memoAuthService] = useMemo(() => [authService], []);
 
-  useEffect(() => {
-    let isSubscribed = true;
+  // useEffect(() => {
+  //   let isSubscribed = true;
 
-    memoAuthService
-      .getUser()
-      .then(info => {
-        // if user is authenticated we can use the authService to snag some user info.
-        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
-        if (isSubscribed) {
-          setUserInfo(info);
-        }
-      })
-      .catch(err => {
-        isSubscribed = false;
-        return setUserInfo(null);
-      });
-    return () => (isSubscribed = false);
-  }, [memoAuthService]);
+  //   memoAuthService
+  //     .getUser()
+  //     .then(info => {
+  //       // if user is authenticated we can use the authService to snag some user info.
+  //       // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
+  //       if (isSubscribed) {
+  //         setUserInfo(info);
+  //       }
+  //     })
+  //     .catch(err => {
+  //       isSubscribed = false;
+  //       return setUserInfo(null);
+  //     });
+  //   return () => (isSubscribed = false);
+  // }, [memoAuthService]);
 
   return (
     <>
-      {authState.isAuthenticated && !userInfo && (
+      <RenderHomePage></RenderHomePage>
+      {/* {authState.isAuthenticated && !userInfo && (
         <LoadingComponent message="Fetching user profile..." />
       )}
       {authState.isAuthenticated && userInfo && (
         <RenderHomePage userInfo={userInfo} authService={authService} />
-      )}
+      )} */}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import {
   BrowserRouter as Router,
   Route,
   useHistory,
-  Switch,
-} from 'react-router-dom';
-import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
+  Switch
+} from "react-router-dom";
+// import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 
-import 'antd/dist/antd.less';
+import "antd/dist/antd.less";
 
-import { NotFoundPage } from './components/pages/NotFound';
-import { ExampleListPage } from './components/pages/ExampleList';
-import { ProfileListPage } from './components/pages/ProfileList';
-import { LoginPage } from './components/pages/Login';
-import { HomePage } from './components/pages/Home';
-import { ExampleDataViz } from './components/pages/ExampleDataViz';
-import { config } from './utils/oktaConfig';
-import { LoadingComponent } from './components/common';
+import { NotFoundPage } from "./components/pages/NotFound";
+import { ExampleListPage } from "./components/pages/ExampleList";
+import { ProfileListPage } from "./components/pages/ProfileList";
+import { LoginPage } from "./components/pages/Login";
+import { NavPage } from "./components/pages/Nav";
+import { HomePage } from "./components/pages/Home";
+import { ExampleDataViz } from "./components/pages/ExampleDataViz";
+// import { config } from './utils/oktaConfig';
+import { LoadingComponent } from "./components/common";
+// import NavContainer from './components/pages/Nav/NavContainer';
 
 ReactDOM.render(
   <Router>
@@ -25,7 +27,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 function App() {
@@ -33,28 +35,31 @@ function App() {
   // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
   const history = useHistory();
 
-  const authHandler = () => {
-    // We pass this to our <Security /> component that wraps our routes.
-    // It'll automatically check if userToken is available and push back to login if not :)
-    history.push('/login');
-  };
+  // const authHandler = () => {
+  //   // We pass this to our <Security /> component that wraps our routes.
+  //   // It'll automatically check if userToken is available and push back to login if not :)
+  //   history.push('/login');
+  // };
 
   return (
-    <Security {...config} onAuthRequired={authHandler}>
+    // <Security {...config} onAuthRequired={authHandler}>
+    <>
       <Switch>
         <Route path="/login" component={LoginPage} />
-        <Route path="/implicit/callback" component={LoginCallback} />
+        {/* <Route path="/implicit/callback" component={LoginCallback} /> */}
         {/* any of the routes you need secured should be registered as SecureRoutes */}
-        <SecureRoute
+        <Route
           path="/"
           exact
           component={() => <HomePage LoadingComponent={LoadingComponent} />}
         />
-        <SecureRoute path="/example-list" component={ExampleListPage} />
-        <SecureRoute path="/profile-list" component={ProfileListPage} />
-        <SecureRoute path="/datavis" component={ExampleDataViz} />
+        <Route path="/example-list" component={ExampleListPage} />
+        <Route path="/profile-list" component={ProfileListPage} />
+        <Route path="/datavis" component={ExampleDataViz} />
         <Route component={NotFoundPage} />
       </Switch>
-    </Security>
+      <NavPage />
+      {/* </Security> */}
+    </>
   );
 }

42dade7d4784c9da3b301122baf6668265c9a770
David Horstman <davidzilla27@gmail.com>
2020-09-10 13:00:42 -0700

    Fix an error with exports from /contexts and /state not working correctly

--- src/state/index.js
+++ src/state/index.js
@@ -2,6 +2,4 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-export { default as actions } from "./actions";
-
 export const store = createStore(rootReducer, applyMiddleware(thunk));

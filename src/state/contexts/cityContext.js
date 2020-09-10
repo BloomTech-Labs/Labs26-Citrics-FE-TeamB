020be2584e21fe9f6716a2305543545c4cf14692
alanblee <alanbenlee12@gmail.com>
2020-09-10 14:17:55 -0700

    also fixed bug with state context

--- src/state/contexts/cityContext.js
+++ src/state/contexts/cityContext.js
@@ -1,5 +1,3 @@
-export default {
-  ADD_CITY: "ADD_CITY",
-  REMOVE_CITY: "REMOVE_CITY",
-  ADD_CITY_DETAILS: "ADD_CITY_DETAILS"
-};
+export const ADD_CITY = "ADD_CITY";
+export const REMOVE_CITY = "REMOVE_CITY";
+export const ADD_CITY_DETAILS = "ADD_CITY_DETAILS";

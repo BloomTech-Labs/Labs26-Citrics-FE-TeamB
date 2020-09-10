075570db89013515b0a3e36ca5e89df791e340fe
David Horstman <davidzilla27@gmail.com>
2020-09-10 14:29:18 -0700

    Switch from open/close to toggle. Add action

---
+++ src/state/actions/drawerActions.js
@@ -0,0 +1,2 @@
+import { TOGGLE_DRAWER } from "../contexts";
+export const toggleDrawer = () => ({ type: TOGGLE_DRAWER });
--- src/state/contexts/drawerContext.js
+++ src/state/contexts/drawerContext.js
@@ -1,2 +1,1 @@
-export const OPEN_DRAWER = "OPEN_DRAWER";
-export const CLOSE_DRAWER = "CLOSE_DRAWER";
+export const TOGGLE_DRAWER = "TOGGLE_DRAWER";

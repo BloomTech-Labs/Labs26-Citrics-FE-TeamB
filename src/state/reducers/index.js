bf0bc9c3632e7a277ef4a2f7d98f0590b33af3a9
David Horstman <davidzilla27@gmail.com>
2020-09-10 14:31:37 -0700

    Add reducer

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
 import cityReducer from "./cityReducer";
+import drawerReducer from "./drawerReducer";
 
-export default combineReducers({ cities: cityReducer });
+export default combineReducers({ cities: cityReducer, drawer: drawerReducer });

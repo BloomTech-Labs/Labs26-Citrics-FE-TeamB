0072dd2713059449fa470d3172bf3672db4bf579
David Horstman <davidzilla27@gmail.com>
2020-09-14 10:58:25 -0700

    Fixed another issue with selectedCities. Convert IDs to numbers before comparing

--- src/state/reducers/cityReducer.js
+++ src/state/reducers/cityReducer.js
@@ -1,7 +1,7 @@
 import { ADD_CITY, REMOVE_CITY, ADD_CITY_DETAILS } from "../contexts";
 
 const initialState = {
-  selectedCities: [{ id: "100", name: "Los Angeles", state: "CA" }],
+  selectedCities: [{ id: 100, name: "Los Angeles", state: "CA" }],
   cityDetails: {}
 };
 export default function cityReducer(state = initialState, { type, payload }) {
@@ -15,12 +15,15 @@ export default function cityReducer(state = initialState, { type, payload }) {
         selectedCities: [...state.selectedCities, payload.city]
       };
     case REMOVE_CITY:
+      // console.log("Removing city number", payload.cityId);
       // To remove details, first copy the object, then remove the key
-      let newDetails = state.cityDetails;
+      let newDetails = { ...state.cityDetails };
       delete newDetails[payload.cityId];
       return {
         ...state,
-        cities: state.selectedCities.filter(city => city.id !== payload.cityId),
+        selectedCities: state.selectedCities.filter(
+          city => Number(city.id) !== Number(payload.cityId)
+        ),
         cityDetails: newDetails
       };
     case ADD_CITY_DETAILS:

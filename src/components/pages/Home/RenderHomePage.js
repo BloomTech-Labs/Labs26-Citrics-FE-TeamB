abd6694cbb3babe17c77e08283a3e3ba0b406213
Rachele Edwards <dot.com1472@gmail.com>
2020-09-14 15:38:01 -0600

    added aria to RenderPage for testing

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,17 +1,29 @@
+//Library imports
 import React from "react";
+import { Typography, Card, Button } from "antd";
+import { connect } from "react-redux";
+import { toggleDrawer } from "../../../state/actions";
+
+//Styling
 import "./styles.css";
-import { Typography, Card } from "antd";
 const { Title, Text } = Typography;
 
-function RenderHomePage(props) {
+function RenderHomePage({ toggleDrawer }) {
+  const openNav = () => {
+    // console.log("Opening Nav");
+    toggleDrawer();
+  };
   return (
     <>
       <div className="background-image" role="img" />
       <Card className="title-card">
         <Title>Citrics</Title>
         <Text>Your one stop for city metrics!</Text>
+        <Button type="primary" onClick={openNav}>
+          Get Started
+        </Button>
       </Card>
     </>
   );
 }
-export default RenderHomePage;
+export default connect(null, { toggleDrawer })(RenderHomePage);

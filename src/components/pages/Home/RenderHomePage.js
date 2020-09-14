e4c44a23bc56870ce15902e4c1aa15b7323419e4
David Horstman <davidzilla27@gmail.com>
2020-09-14 09:51:55 -0700

    Add search bar opening button to HomePage

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,17 +1,30 @@
+//Library imports
 import React from "react";
+import { Typography, Card, Button } from "antd";
+import { connect } from "react-redux";
+import { toggleDrawer } from "../../../state/actions";
+
+//Styling
 import "./styles.css";
-import { Typography, Card } from "antd";
 const { Title, Text } = Typography;
 
-function RenderHomePage(props) {
+function RenderHomePage({ toggleDrawer }) {
+  const openNav = () => {
+    // console.log("Opening Nav");
+    toggleDrawer();
+  };
   return (
     <>
       <div className="background-image" />
       <Card className="title-card">
         <Title>Citrics</Title>
         <Text>Your one stop for city metrics!</Text>
+        <Button type="primary" onClick={openNav}>
+          Get Started
+        </Button>
       </Card>
     </>
   );
 }
-export default RenderHomePage;
+const mapPropsToState = (reduxProps, props) => props;
+export default connect(mapPropsToState, { toggleDrawer })(RenderHomePage);

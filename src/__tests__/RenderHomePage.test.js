cd2eadf42ea9005eef1c21da886c0e232971a69e
Rachele Edwards <dot.com1472@gmail.com>
2020-09-14 15:05:43 -0600

    adding dummy data to authen test folders and adding tests with RenderHomePage

--- src/__tests__/RenderHomePage.test.js
+++ src/__tests__/RenderHomePage.test.js
@@ -1,9 +1,11 @@
 import RenderHomePage from "../components/pages/Home/RenderHomePage";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { getByText, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { store } from "../state";
 
 describe("<RenderHomePage /> test suite", () => {
   //COMMENTING OUT DUE TO NOT HAVING LOGIN/AUTHENTICATION IN RELEASE 1--KEEPING IF NEEDED IN RELEASE 2
@@ -27,19 +29,44 @@ describe("<RenderHomePage /> test suite", () => {
   // Arrange - Act - Assert pattern
   test("Renders homepage without errors", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<RenderHomePage />, div);
+    ReactDOM.render(
+      <Provider store={store}>
+        <RenderHomePage />
+      </Provider>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test('If header title "Citrics" is on the document', async () => {
-    const { findByText } = render(<RenderHomePage />);
+    const { findByText } = render(
+      <Provider store={store}>
+        <RenderHomePage />
+      </Provider>
+    );
     const showHeader = await findByText(/Citrics/i);
     expect(showHeader).toBeInTheDocument();
   });
 
-  test("If landingpage has a background image", () => {});
+  test("If landingpage has a background image", () => {
+    const { getByRole } = render(
+      <Provider store={store}>
+        <RenderHomePage />
+      </Provider>
+    );
+    const bgImage = getByRole("img");
+    expect(bgImage).toBeInTheDocument();
+  });
 
-  test("If Get Started Button is clickable", () => {});
+  test("If Get Started Button is clickable", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <RenderHomePage />
+      </Provider>
+    );
+    const getStartedBtn = getByText(/Get Started/i);
+    expect(getStartedBtn).toBeInTheDocument();
+  });
 
   test("If clicking Get Started button opens the Nav Bar", () => {});
 });
--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,29 +1,17 @@
-//Library imports
 import React from "react";
-import { Typography, Card, Button } from "antd";
-import { connect } from "react-redux";
-import { toggleDrawer } from "../../../state/actions";
-
-//Styling
 import "./styles.css";
+import { Typography, Card } from "antd";
 const { Title, Text } = Typography;
 
-function RenderHomePage({ toggleDrawer }) {
-  const openNav = () => {
-    // console.log("Opening Nav");
-    toggleDrawer();
-  };
+function RenderHomePage(props) {
   return (
     <>
-      <div className="background-image" />
+      <div className="background-image" role="img" />
       <Card className="title-card">
         <Title>Citrics</Title>
         <Text>Your one stop for city metrics!</Text>
-        <Button type="primary" onClick={openNav}>
-          Get Started
-        </Button>
       </Card>
     </>
   );
 }
-export default connect(null, { toggleDrawer })(RenderHomePage);
+export default RenderHomePage;

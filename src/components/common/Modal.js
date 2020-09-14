3f9391d34f17c1ff45466c8d31eddcd7feb10752
alanblee <alanbenlee12@gmail.com>
2020-09-14 14:02:35 -0700

    modal is connected to use global redux state for displaying details for single cities

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Modal } from "antd";
+import { connect } from "react-redux";
 import { CityDetailPage } from "../pages/CityDetail";
 
-export default function ModalComponent({ visible, setVisible, city }) {
+const ModalComponent = ({ visible, setVisible, singleCity }) => {
+  const [city, setCity] = useState(singleCity);
+  useEffect(() => {
+    setCity(singleCity);
+  }, [singleCity]);
   return (
     <>
-      <Modal
-        title={`Info for ${city.name}`}
-        centered
-        visible={visible}
-        onOk={() => setVisible(false)}
-        onCancel={() => setVisible(false)}
-        style={{ top: 20 }}
-        width={1000}
-      >
-        <CityDetailPage city={city} />
-      </Modal>
+      {city ? (
+        <Modal
+          title={`Info for ${city.name}`}
+          centered
+          visible={visible}
+          onOk={() => setVisible(false)}
+          onCancel={() => setVisible(false)}
+          style={{ top: 20 }}
+          width={1000}
+        >
+          <CityDetailPage city={city} />
+        </Modal>
+      ) : null}
     </>
   );
-}
+};
+
+const mapState = state => ({
+  singleCity: state.cities.singleCityDetails
+});
+
+export default connect(mapState, null)(ModalComponent);

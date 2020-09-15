d327794cf9a449dd583c20ec69ca25c307f50e69
Rachele Edwards <dot.com1472@gmail.com>
2020-09-15 15:12:25 -0600

    added color scheme and fonts to comments section of override--planning out styling

--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -1,16 +1,31 @@
 module.exports = {
-  '@primary-color': '#ec3944',
-  '@link-color': ' #ec3944',
-  '@success-color': '#52c41a',
-  '@warning-color': '#faad14',
-  '@error-color': '#f5222d',
-  '@font-size-base': '14px',
-  '@heading-color': 'rgba(0, 0, 0, 0.85)',
-  '@text-color': 'rgba(0, 0, 0, 0.65)',
-  '@text-color-secondary': 'rgba(0, 0, 0, 0.45)',
-  '@disabled-color': 'rgba(0, 0, 0, 0.25)',
-  '@border-radius-base': '2px',
-  '@border-color-base': '#d9d9d9',
-  '@box-shadow-base':
-    '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
+  "@primary-color": "#EB4B3D",
+  "@link-color": " #057CD8",
+  "@success-color": "#057CD8",
+  "@warning-color": "#FFD700",
+  "@error-color": "#f5222d",
+  "@font-size-base": "14px",
+  "@heading-color": "rgba(0, 0, 0, 0.85)",
+  "@text-color": "rgba(0, 0, 0, 0.65)",
+  "@text-color-secondary": "rgba(0, 0, 0, 0.45)",
+  "@disabled-color": "rgba(0, 0, 0, 0.25)",
+  "@border-radius-base": "2px",
+  "@border-color-base": "#d9d9d9",
+  "@box-shadow-base":
+    "0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)"
 };
+
+//Color Scheme:
+//Bright Navy Blue: #057CD8
+//Bright Gold: #FFD700
+//Orange Soda: #E36242
+//Cinnabar Red: #EB4B3D
+//Light Yellow: #FEE885
+//Dark Gray: #777A66
+//Timberwolf Gray: #D2D4C9
+
+//Font Family: Josefin Sans
+//Light: Light 300
+//Regular: Regular 400
+//Bold: Bold 700
+//Italic: " " + Italic

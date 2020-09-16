ddd5912e89a9b8ed7f8ffdde71e80579cbc6c075
alanblee <alanbenlee12@gmail.com>
2020-09-16 12:43:43 -0700

    gets the city to render from props or a router push

--- src/components/pages/CityDetail/RenderCityDetail.js
+++ src/components/pages/CityDetail/RenderCityDetail.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import GraphContainer from "../../common/GraphContainer";
 
 export default function RenderCityDetail({ city }) {
+  // This piece of code will determine whether the selected city will come from a modal or is pushed through route
+  const location = useLocation();
+  const [citySelect, setCity] = useState(city || location.state);
+  useEffect(() => {
+    setCity(city || location.state);
+  }, [location, city]);
+
   return (
     <div>
-      <p>City: {city.name}</p>
-      <p> State: {city.state}</p>
-      <p>Rent: {city.rental}</p>
-      <p>Population: {city.pop}</p>
-      <p>Weather: {city.weather}</p>
-      <GraphContainer state={city.state} />
+      <p>City: {citySelect.name}</p>
+      <p> State: {citySelect.state}</p>
+      {/* <p>Rent: {citySelect.rental}</p>
+      <p>Population: {citySelect.pop}</p>
+      <p>Weather: {citySelect.weather}</p> */}
+      <GraphContainer state={citySelect.state} />
     </div>
   );
 }

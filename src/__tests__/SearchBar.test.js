22aab0f443ace55bdc3f3a200fd2f5fc1c226eea
David Horstman <davidzilla27@gmail.com>
2020-09-16 14:18:35 -0700

    Start writing tests for SearchBar

--- src/__tests__/SearchBar.test.js
+++ src/__tests__/SearchBar.test.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { render, fireEvent } from "@testing-library/react";
 import configureStore from "redux-mock-store";
-import { removeCity } from "../state/actions";
-import SelectedCities from "../components/pages/Nav/SelectedCities";
+import { addCity } from "../state/actions";
+import SearchBar from "../components/pages/Nav/SearchBar";
 const mockStore = configureStore([]);
 // A list of dummy data to get the search form functional
 // Data pulled from: https://simple.wikipedia.org/wiki/List_of_United_States_cities_by_population#Cities_that_used_to_have_100,000_people
@@ -39,68 +39,32 @@ const cities = [
   { id: 28, name: "Youngstown", state: "OH" }
 ];
 describe("<SearchBar />", () => {
-  it("exists", () => {
-    expect(true).toBe(true);
+  let store, component;
+  beforeEach(() => {
+    store = mockStore({
+      cities: {
+        selectedCities: [
+          { id: 1, name: "Albany", state: "NY" },
+          { id: 2, name: "Allegheny", state: "PA" },
+          { id: 3, name: "Brooklyn", state: "NY" }
+        ]
+      }
+    });
+    store.dispatch = jest.fn();
+    component = render(
+      <Provider store={store}>
+        <SearchBar />
+      </Provider>
+    );
+  });
+  it("Renders navbar without errors", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <Provider store={store}>
+        <SearchBar />
+      </Provider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
   });
 });
-// describe("<NavContainer />", () => {
-//   let store, component;
-//   beforeEach(() => {
-//     store = mockStore({
-//       cities: {
-//         selectedCities: [
-//           { id: 1, name: "Albany", state: "NY" },
-//           { id: 2, name: "Allegheny", state: "PA" },
-//           { id: 3, name: "Brooklyn", state: "NY" },
-//         ],
-//       },
-//     });
-//     store.dispatch = jest.fn();
-//     component = render(
-//       <Provider store={store}>
-//         <SelectedCities />
-//       </Provider>
-//     );
-//   });
-
-//   it("Renders navbar without errors", () => {
-//     const div = document.createElement("div");
-//     ReactDOM.render(
-//       <Provider store={store}>
-//         <SelectedCities />
-//       </Provider>,
-//       div
-//     );
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-//   it("Is hidden when no cities are selected", async done => {
-//     store = mockStore({
-//       cities: {
-//         selectedCities: [],
-//       },
-//     });
-//     store.dispatch = jest.fn();
-//     component = render(
-//       <Provider store={store}>
-//         <SelectedCities />
-//       </Provider>
-//     );
-//     const { findByTestId, queryByText } = component;
-//     const visibilityButton = await findByTestId("floating-visibility-button");
-//     expect(visibilityButton).toBeInTheDocument();
-//     const header = queryByText(/city search/i);
-//     expect(header).toBeNull();
-//     done();
-//   });
-//   it("Opens and closes when the button is pressed", async done => {
-//     const { findByTestId } = component;
-//     const visibilityButton = await findByTestId("floating-visibility-button");
-//     fireEvent.click(visibilityButton);
-//     expect(store.dispatch).toHaveBeenCalledTimes(1);
-//     expect(store.dispatch).toHaveBeenCalledWith(toggleDrawer());
-//     fireEvent.click(visibilityButton);
-//     expect(store.dispatch).toHaveBeenCalledTimes(2);
-//     expect(store.dispatch).toHaveBeenCalledWith(toggleDrawer());
-//     done();
-//   });
-// });

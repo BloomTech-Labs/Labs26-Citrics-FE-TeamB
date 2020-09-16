7d12f45caa121dd1b2e6f8b99c5fdfd95fec35ef
alanblee <alanbenlee12@gmail.com>
2020-09-15 19:56:40 -0700

    mocked props to render cards for testing. mocked button click to check for state change. mocked button click to render the modal

--- src/__tests__/RenderComparisonPage.test.js
+++ src/__tests__/RenderComparisonPage.test.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { render, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
-import axios from "axios";
 import configureStore from "redux-mock-store";
 import RenderComparison from "../components/pages/Comparison/RenderComparison";
-// import GraphContainer from "../components/common/GraphContainer";
 
 const mockStore = configureStore([]);
 let store, component;
@@ -37,22 +35,64 @@ beforeEach(() => {
       }
     }
   });
-  store.dispatch = jest.fn();
-  jest.mock("axios");
   component = render(
     <Provider store={store}>
-      <RenderComparison />
+      <RenderComparison citiesData={store.getState().cities.cityDetails} />
     </Provider>
   );
 });
 // Renders the comparison page
 it("Renders the comparison without errors", () => {
   const div = document.createElement("div");
-
   ReactDOM.render(
     <Provider store={store}>
       <RenderComparison />
     </Provider>,
     div
   );
+});
+
+it("renders the city cards to the page", () => {
+  const { getAllByTestId } = component;
+  const CityCards = getAllByTestId("city-cards");
+  CityCards.forEach(ele => {
+    expect(ele).toBeInTheDocument();
+  });
+});
+
+it("renders the view more info button for each card", () => {
+  const { getAllByTestId } = component;
+  const InfoBtn = getAllByTestId("more-info-btn");
+  InfoBtn.forEach(ele => {
+    expect(ele).toBeInTheDocument();
+  });
+});
+
+it("changes visit state to true when btn is clicked", () => {
+  // mock the function
+  const setVisible = jest.fn();
+  const handleClick = jest.spyOn(React, "useState");
+  handleClick.mockImplementation(visible => [visible, setVisible]);
+
+  const { getAllByTestId } = component;
+  const InfoBtn = getAllByTestId("more-info-btn");
+
+  fireEvent.click(InfoBtn[0]);
+  expect(setVisible).toBeTruthy();
+});
+
+it("displays the detailed city page after the button is clicked", () => {
+  // mock the function
+  const setVisible = jest.fn();
+  const handleClick = jest.spyOn(React, "useState");
+  handleClick.mockImplementation(visible => [visible, setVisible]);
+
+  const { getAllByTestId } = component;
+  const InfoBtn = getAllByTestId("more-info-btn");
+  fireEvent.click(InfoBtn[0]);
+
+  // gets updated dom elements after button is clicked
+  const { getByTestId } = component;
+  const CityDetail = getByTestId("city-details");
+  expect(CityDetail).toBeInTheDocument();
 });

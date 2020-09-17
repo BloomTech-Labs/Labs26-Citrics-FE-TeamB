71e445f228fcbaaab229d9310fac84a9dcb847f6
David Horstman <davidzilla27@gmail.com>
2020-09-17 15:25:56 -0700

    Update comments

--- src/components/pages/Nav/SearchBar.js
+++ src/components/pages/Nav/SearchBar.js
@@ -11,7 +11,6 @@ class SearchBar extends React.Component {
     axios
       .get("https://b-ds.citrics.dev/cities")
       .then(r => r.data.cities)
-      // .then(r => {console.log(r); return r;})
       .then(queryResult =>
         this.setState({
           cityList: queryResult.map(item => ({
@@ -22,24 +21,20 @@ class SearchBar extends React.Component {
       );
   }
 
-  // Sets user input to searchTerm
+  // Every keystroke, update what's shown in the box
+  // and generate an autocomplete
   onChange = searchTerm => {
     this.setState({ searchTerm });
     const searchResults = this.state.cityList.filter(item =>
       item.value.toLowerCase().includes(searchTerm)
     );
-    // this.setState({ searchResults });
     this.setState({
       options: !this.state.searchTerm ? [] : searchResults.slice(0, 5)
     });
   };
 
-  // // On each keystroke, update the list shown in Autocomplete
-  // onSearch = searchText => {
-
-  // };
-
   // When clicking on a city, add it to the selected cities
+  // and empty the search term
   onSelect = data => {
     const entry = this.state.cityList.find(({ value }) => value === data);
     this.props.addCity(entry);
@@ -52,7 +47,6 @@ class SearchBar extends React.Component {
         options={this.state.options}
         style={{ width: 200 }}
         onSelect={this.onSelect}
-        // onSearch={this.onSearch}
         onChange={this.onChange}
         placeholder="Enter City.."
       />

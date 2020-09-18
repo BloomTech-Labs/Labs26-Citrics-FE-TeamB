9fd54831bb99539b1ebad71415e51b90c2a3506b
David Horstman <davidzilla27@gmail.com>
2020-09-18 14:44:10 -0700

    Refactor to class component and split selectedCity and cityData logic

--- src/components/pages/Comparison/ComparisonContainer.js
+++ src/components/pages/Comparison/ComparisonContainer.js
@@ -5,24 +5,46 @@ import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { getCityDetails } from "../../../state/actions";
 
-function ComparisonContainer({
-  getCityDetails,
-  cityDetails,
-  location: { search }
-}) {
-  const queryParams = new URLSearchParams(search);
-  const selectedCities = Array.from(queryParams.values()).map(id => Number(id));
-  console.log("selectedCities", selectedCities);
-  console.log("Details", cityDetails);
-  const citiesData = selectedCities.map(id => cityDetails[id]);
-  console.log("citiesData", citiesData);
-  return citiesData.length < 2 ? (
-    <Redirect to="/" />
-  ) : (
-    <div className="comparison-container">
-      <RenderComparison citiesData={citiesData} />
-    </div>
-  );
+class ComparisonContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCities: this.parseSelectedCities(props),
+      citiesData: []
+    };
+  }
+  componentDidMount() {
+    this.retrieveCityDataIfNeeded(this.state.selectedCities);
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.search !== this.props.location.search) {
+      const selectedCities = this.parseSelectedCities(this.props);
+      this.setState({ selectedCities });
+      this.retrieveCityDataIfNeeded(selectedCities);
+    }
+  }
+  parseSelectedCities = props => {
+    const queryParams = new URLSearchParams(props.location.search);
+    const selectedCities = Array.from(queryParams.values()).map(id =>
+      Number(id)
+    );
+    console.log("selectedCities", selectedCities);
+    return selectedCities;
+  };
+  retrieveCityDataIfNeeded = selectedCities => {
+    const citiesData = selectedCities.map(id => this.props.cityDetails[id]);
+    console.log("citiesData", citiesData);
+    this.setState({ citiesData });
+  };
+  render() {
+    return this.state.selectedCities.length < 2 ? (
+      <Redirect to="/" />
+    ) : (
+      <div className="comparison-container">
+        <RenderComparison citiesData={this.state.citiesData} />
+      </div>
+    );
+  }
 }
 const mapState = ({ cities: { cityDetails } }, props) => ({
   ...props,

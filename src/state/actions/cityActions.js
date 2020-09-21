4de03425e2e934d7a8fdf3fb957d9420573a226a
alanblee <alanbenlee12@gmail.com>
2020-09-21 15:09:51 -0700

    refactoring the getCityDetails to use async axios calls to DS endpoint

--- src/state/actions/cityActions.js
+++ src/state/actions/cityActions.js
@@ -1,5 +1,5 @@
 import { ADD_CITY, REMOVE_CITY, ADD_CITY_DETAILS } from "../contexts";
-//import axios from "axios";
+import axios from "axios";
 
 export const addCity = city => async dispatch => {
   dispatch({
@@ -8,24 +8,32 @@ export const addCity = city => async dispatch => {
   });
   // changing the param to the entire city for now just to test actions
   // once we have the proper endpoints will change back
-  dispatch(getCityDetails(city));
+  getCityDetails(city);
 };
 export const removeCity = cityId => ({
   type: REMOVE_CITY,
   payload: { cityId }
 });
 
-export const getCityDetails = ({ id, name, state }) => {
+export const getCityDetails = ({ id, name, state }) => async dispatch => {
+  // this is just a test
+  let stateName = state ? state : "CA";
+  const unemploymentRate = await axios.get(
+    `https://b-ds.citrics.dev/viz/${stateName}`
+  );
+  console.log(JSON.parse(unemploymentRate.data).data);
   // TODO: Add API call here to get detailed data
   const details = {
     population: 100,
     weather: "perfect",
     rent: 10000,
-    name: name ?? "Example City",
-    state: state ?? "CA"
+    name,
+    state: stateName,
+    unemployRate: JSON.parse(unemploymentRate.data).data[0]
   };
-  return {
+
+  dispatch({
     type: ADD_CITY_DETAILS,
     payload: { id, details }
-  };
+  });
 };

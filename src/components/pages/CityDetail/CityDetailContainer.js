e54052439759eee4e96664a39c17e5a2de2a7139
Rachele Edwards <dot.com1472@gmail.com>
2020-09-21 15:13:38 -0600

    added class to style city details card

--- src/components/pages/CityDetail/CityDetailContainer.js
+++ src/components/pages/CityDetail/CityDetailContainer.js
@@ -4,7 +4,7 @@ import RenderCityDetail from "./RenderCityDetail";
 
 export default function CityDetailContainer({ city }) {
   return (
-    <div data-testid="city-details">
+    <div className="city-details" data-testid="city-details">
       <RenderCityDetail city={city} />
     </div>
   );

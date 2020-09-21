3d6ae5f982661db9c8355a9a10ab60c31f4ba274
David Horstman <davidzilla27@gmail.com>
2020-09-21 14:38:23 -0700

    Split Nav into two components after all

---
+++ src/components/pages/Nav/RenderNav.js
@@ -0,0 +1,48 @@
+// Library imports
+import React from "react";
+import { Drawer, Button } from "antd";
+import { DoubleLeftOutlined, DoubleRightOutlined } from "@ant-design/icons";
+
+//Subcomponents
+import { SearchBar } from "../SearchBar";
+import { SelectedCities } from "../SelectedCities/";
+
+//Configuration
+// This defines the width of the drawer *and* how far to translate the floating button
+import drawerWidth from "./drawerWidth";
+
+export default function RenderNav({ toggleDrawer, isOpen }) {
+  const buttonTransform = isOpen ? `translate(${drawerWidth}px,0px)` : "";
+
+  return (
+    <div className="navbar">
+      <Button
+        className="floating-visibility-button"
+        data-testid="floating-visibility-button"
+        type="secondary"
+        onClick={toggleDrawer}
+        style={{ transform: buttonTransform }}
+      >
+        {isOpen ? <DoubleLeftOutlined /> : <DoubleRightOutlined />}
+      </Button>
+      <Drawer
+        title="City Search"
+        placement="left"
+        closable={false}
+        onClose={toggleDrawer}
+        visible={isOpen}
+        mask={false}
+        width={drawerWidth}
+      >
+        <SearchBar />
+        {/* TODO: remove these and clean up formatting of this element */}
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <SelectedCities />
+      </Drawer>
+    </div>
+  );
+}

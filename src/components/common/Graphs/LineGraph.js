2bbc1afe790dd5a9b27a5a7a9bd666619800046f
alanblee <alanbenlee12@gmail.com>
2020-09-21 16:56:59 -0700

    updated graph useEffect to not use axios calls

--- src/components/common/Graphs/LineGraph.js
+++ src/components/common/Graphs/LineGraph.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import Plot from "react-plotly.js";
 import LoadingComponent from "../../common/LoadingComponent";
 
@@ -9,18 +8,13 @@ export default function LineGraph({ state, state2, state3 }) {
   const [data3, setData3] = React.useState(null);
 
   React.useEffect(() => {
-    axios
-      .get(`https://b-ds.citrics.dev/viz/${state}`)
-      .then(({ data }) => setData(JSON.parse(data).data));
+    console.log(state, state3, state3);
+    setData(state);
     if (state2) {
-      axios
-        .get(`https://b-ds.citrics.dev/viz/${state2}`)
-        .then(({ data }) => setData2(JSON.parse(data).data));
+      setData2(state2);
     }
     if (state3) {
-      axios
-        .get(`https://b-ds.citrics.dev/viz/${state3}`)
-        .then(({ data }) => setData3(JSON.parse(data).data));
+      setData3(state3);
     }
   }, [state, state2, state3]);
 
@@ -28,29 +22,29 @@ export default function LineGraph({ state, state2, state3 }) {
     let trace2, trace3;
 
     let trace1 = {
-      x: data[0].x,
-      y: data[0].y,
+      x: data.plotX,
+      y: data.plotY,
       line: { color: "rgba(222,45,38,0.8)" },
       mode: "lines",
-      name: state,
+      name: data.state,
       type: "scatter"
     };
     if (data2) {
       trace2 = {
-        x: data2[0].x,
-        y: data2[0].y,
+        x: data2.plotX,
+        y: data2.plotY,
         line: { color: "rgb(49,130,189)" },
         mode: "lines",
-        name: state2 ? state2 : "State 2"
+        name: state2 ? data2.state : "Fix me"
       };
     }
     if (data3) {
       trace3 = {
-        x: data3[0].x,
-        y: data3[0].y,
+        x: data3.plotX,
+        y: data3.plotY,
         line: { color: "rgb(204,204,204)" },
         mode: "lines",
-        name: state3 ? state3 : "state 3"
+        name: state3 ? data3.state : "Fix me too"
       };
     }
 

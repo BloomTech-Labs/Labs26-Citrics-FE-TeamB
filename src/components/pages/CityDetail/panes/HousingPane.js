ef97a4bdc006d97f896220dc05bd740a0fd424c1
David Horstman <davidzilla27@gmail.com>
2020-09-22 15:45:39 -0700

    Add rental prices tab bar

--- src/components/pages/CityDetail/panes/HousingPane.js
+++ src/components/pages/CityDetail/panes/HousingPane.js
@@ -1,5 +1,15 @@
 import React from "react";
-
+import { Tabs } from "antd";
 export default function HousingPane({ rent }) {
-  return <>Rental Prices: ${rent.studio}</>;
+  const { TabPane } = Tabs;
+  const aptTypes = ["Studio", "1BR", "2BR", "3BR", "4BR"];
+  return (
+    <Tabs defaultActiveKey="0">
+      {aptTypes.map((name, idx) => (
+        <TabPane key={idx} tab={name} className="rental-price-tab">
+          ${rent[name.toLowerCase()]}
+        </TabPane>
+      ))}
+    </Tabs>
+  );
 }

d9148e26777d035118cf25df6e615350bf89e996
David Horstman <davidzilla27@gmail.com>
2020-09-22 15:03:11 -0700

    Add hideable graph pane

--- src/components/pages/CityDetail/panes/DetailPane.js
+++ src/components/pages/CityDetail/panes/DetailPane.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Graph from "../../../common/Graphs/renderGraph";
+// import { Collapse } from "antd";
+import { RightOutlined } from "@ant-design/icons";
 
-export default function DetailPane({ Icon, graphData, children }) {
+export default function DetailPane({ Icon, GraphIcon, graphData, children }) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleGraph = () => {
+    console.log("Toggle");
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="one-render-p">
       <Icon />
       {
         children /*This is where the child component (a specific type of pane) is rendered.*/
       }
-      {/* TODO: Show and hide graph */}
-      {graphData && <Graph dataSet={graphData} />}
+      <br />
+      {graphData && (
+        <div className={"graph-icons"}>
+          <RightOutlined
+            onClick={toggleGraph}
+            className="graph-toggle"
+            rotate={isOpen ? 90 : 0}
+          />
+          {isOpen ? graphData.graphName : <GraphIcon className="graph-icon" />}
+        </div>
+      )}
+      {isOpen && <Graph dataSet={graphData} />}
     </div>
   );
 }

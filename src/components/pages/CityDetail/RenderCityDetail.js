200bffe41e501d7aeeb03ccf4956dcd88b5c3b18
alanblee <alanbenlee12@gmail.com>
2020-09-21 20:03:53 -0700

    rendering actual population with graph

--- src/components/pages/CityDetail/RenderCityDetail.js
+++ src/components/pages/CityDetail/RenderCityDetail.js
@@ -7,14 +7,26 @@ export default function RenderCityDetail({ city }) {
       <h3>
         City: {city.name}, {city.state}
       </h3>
-      <p>Population: {city.population}</p>
+      <p>Population: {city.population.data.total_pop}</p>
       <p>Rental Prices: ${city.rent}</p>
       <p>Weather: {city.weather}</p>
+
+      {/* Unemployment Graph */}
       <LineGraph
         state={{
           state: city.state,
           plotX: city.unemployRate.x,
-          plotY: city.unemployRate.y
+          plotY: city.unemployRate.y,
+          graphName: "Unemployment Rate"
+        }}
+      />
+      {/* Population Graph */}
+      <LineGraph
+        state={{
+          state: city.state,
+          plotX: JSON.parse(city.population.viz).data[0].x,
+          plotY: JSON.parse(city.population.viz).data[0].y,
+          graphName: "Population Trend"
         }}
       />
     </div>

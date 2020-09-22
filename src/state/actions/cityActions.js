c8e3648431a3bd996b0915897dab19f8069a744f
David Horstman <davidzilla27@gmail.com>
2020-09-21 21:24:08 -0700

    Hotfix - switch to dedicated proxy server

--- src/state/actions/cityActions.js
+++ src/state/actions/cityActions.js
@@ -6,8 +6,7 @@ export const addCity = city => async dispatch => {
     type: ADD_CITY,
     payload: { city }
   });
-  // changing the param to the entire city for now just to test actions
-  // once we have the proper endpoints will change back
+  // I'm not 100% sure why wrapping this in dispatch is necessary, but it is
   dispatch(getCityDetails(city));
 };
 export const removeCity = cityId => ({
@@ -15,9 +14,15 @@ export const removeCity = cityId => ({
   payload: { cityId }
 });
 
-export const getCityDetails = ({ id, name, state }) => async dispatch => {
-  //Placeholder image for cities with no Places API image result
-  const fallbackImage = "https://i.imgur.com/YXdssOR.jpeg";
+export const getCityDetails = city => async (dispatch, getState) => {
+  let { id, name, state } = city;
+
+  // Do nothing if we already have the details on this city
+  const { cityDetails } = getState().cities;
+  if (cityDetails[id]) return;
+
+  //Placeholder image to use as a fallback
+  let image = "https://i.imgur.com/YXdssOR.jpeg";
 
   // If we aren't given name and state, look them up from the backend
   if (!(name && state)) {
@@ -27,11 +32,13 @@ export const getCityDetails = ({ id, name, state }) => async dispatch => {
     const city = cityList.find(
       ({ id: cityId }) => Number(cityId) === Number(id)
     );
-    // console.log(city);
     name = city?.name;
     state = city?.state;
   }
-  const proxyURL = "https://cors-anywhere.herokuapp.com/";
+  // All requests are routed thru this proxy to circumvent CORS issues
+  const proxyURL = "https://cors-anywhere-citrics.herokuapp.com/";
+
+  // Initial places lookup request gives up a photo ref for the given city
   const placesLookupURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${name} ${state}&key=${process.env.REACT_APP_PLACES_API_KEY}&inputtype=textquery&fields=name,photos`;
   const initialImageQuery = await axios
     .get(proxyURL + placesLookupURL)
@@ -39,12 +46,17 @@ export const getCityDetails = ({ id, name, state }) => async dispatch => {
   const photoRef =
     initialImageQuery?.data?.candidates?.[0]?.photos?.[0]?.photo_reference;
 
-  const imageLookupURL = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoRef}&key=${process.env.REACT_APP_PLACES_API_KEY}&maxwidth=700&maxheight=700`;
-  const imageURLQuery = await fetch(proxyURL + imageLookupURL)
-    .then(r => r.blob())
-    .catch(console.error);
-
-  const image = photoRef ? URL.createObjectURL(imageURLQuery) : fallbackImage;
+  // If we succeeded in getting a photo ref, get the image
+  // if it failed, image will instead be the placeholder above
+  if (photoRef) {
+    const imageLookupURL = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoRef}&key=${process.env.REACT_APP_PLACES_API_KEY}&maxwidth=700&maxheight=700`;
+    // Data is sent as a blob, which axios handles poorly, hence the use of fetch
+    const imageURLQuery = await fetch(proxyURL + imageLookupURL)
+      .then(r => r.blob())
+      .catch(console.error);
+    // Create a url for the image to be compatible with <img src='' />
+    image = URL.createObjectURL(imageURLQuery);
+  }
   const details = {
     population: 100,
     weather: "perfect",

2aad4510906239a6845afc41465061dd4274582d
David Horstman <davidzilla27@gmail.com>
2020-09-22 16:12:59 -0700

    Default to 1BR instead of studio

--- src/components/pages/CityDetail/panes/HousingPane.js
+++ src/components/pages/CityDetail/panes/HousingPane.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Tabs } from "antd";
 export default function HousingPane({ rent }) {
+  console.log(rent);
   const { TabPane } = Tabs;
   // This list will be the titles of the tabs
   // it also matches the keys in rent (although they're all lowercase in rent)
   const aptTypes = ["Studio", "1BR", "2BR", "3BR", "4BR"];
   return (
-    <Tabs defaultActiveKey="0">
+    <Tabs defaultActiveKey="1">
       {aptTypes.map((name, idx) => (
         <TabPane key={idx} tab={name} className="rental-price-tab">
           ${rent[name.toLowerCase()]}

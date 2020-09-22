a30f21654470ec044ec7c94adffcc94671c109cb
alanblee <alanbenlee12@gmail.com>
2020-09-22 16:08:06 -0700

    updated modal props and refactored comparison page. can probably change it back to functional component

--- src/components/pages/Comparison/RenderComparison.js
+++ src/components/pages/Comparison/RenderComparison.js
@@ -1,91 +1,32 @@
-import React, { Component, useState } from "react";
-import { Card, Button, Tabs } from "antd";
-import ModalComponent from "../../common/Modal.js";
+import React, { Component } from "react";
+import { Tabs } from "antd";
 import LoadingComponent from "../../common/LoadingComponent.js";
-// Graph
+import ComparisonCard from "./comparisonCard";
 import Graph from "../../common/Graphs/renderGraph";
 
-export default function RenderComparison({ citiesData }) {
-  const [visible, setVisible] = useState(false);
-  const [city, setCity] = useState({});
-  const toggleModal = cityData => {
-    setCity(cityData);
-    // Toggles the modal to open
-    setVisible(true);
-  };
-
-  //Ant design tabs
-  const { TabPane } = Tabs;
-  const callback = key => {
-    console.log(key);
-  };
-  // Iterates through the cities state and renders the card per city
-  const renderCard = () => {
-    const cities = [];
-    for (const data in citiesData) {
-      cities.push(
-        <div className="card" key={data}>
-          <Card className="comparison-card" data-testid="city-cards">
-            <div
-              className="custom-image"
-              style={{ backgroundImage: `url(${citiesData[data].image})` }}
-            >
-              <img
-                alt={`Thumbnail for ${citiesData[data].name}, ${citiesData[data].state}`}
-                src={citiesData[data].image}
-              />
-            </div>
-            {!citiesData[data] ? (
-              <LoadingComponent message="Loading city data..." />
-            ) : (
-              <div className="custom-card">
-                <h3>
-                  {citiesData[data].name}, {citiesData[data].state}
-                </h3>
-                <p>Population: {citiesData[data].population.data.total_pop}</p>
-                <p>Rental Prices: ${citiesData[data].rent.studio}</p>
-                <p>
-                  Weather: {citiesData[data].weather.summer_maxtempF_mean}{" "}
-                  degrees
-                </p>
-                <Button
-                  className="more-info-btn"
-                  data-testid="more-info-btn"
-                  type="primary"
-                  onClick={() => toggleModal(citiesData[data])}
-                >
-                  More Info
-                </Button>
-              </div>
-            )}
-          </Card>
-        </div>
-      );
-    }
-    return cities;
-  };
-
-  // funciton to put state names into an array for quick test
-  const getStateName = () => {
+class RenderComparison extends Component {
+  // puts state name in an array for easier acccess
+  getStateName = () => {
     const stateName = [];
-    for (let id in citiesData) {
+    for (let id in this.props.citiesData) {
       stateName.push({
-        state: citiesData[id].state,
-        plotX: citiesData[id].unemployRate.x,
-        plotY: citiesData[id].unemployRate.y,
+        state: this.props.citiesData[id].state,
+        plotX: this.props.citiesData[id].unemployRate.x,
+        plotY: this.props.citiesData[id].unemployRate.y,
         graphName: "Unemployment Rate",
         type: "line"
       });
     }
     return stateName;
   };
-  const getCityPop = () => {
+  // puts plot data  in an array for easier acccess
+  getCityPop = () => {
     const cityPop = [];
-    for (let id in citiesData) {
+    for (let id in this.props.citiesData) {
       cityPop.push({
-        state: citiesData[id].state,
-        plotX: JSON.parse(citiesData[id].population.viz).data[0].x,
-        plotY: JSON.parse(citiesData[id].population.viz).data[0].y,
+        state: this.props.citiesData[id].state,
+        plotX: JSON.parse(this.props.citiesData[id].population.viz).data[0].x,
+        plotY: JSON.parse(this.props.citiesData[id].population.viz).data[0].y,
         graphName: "Population Trend",
         type: "bar"
       });
@@ -93,43 +34,57 @@ export default function RenderComparison({ citiesData }) {
     return cityPop;
   };
 
-  if (citiesData.length === 0) {
-    return <LoadingComponent message="Loading city data..." />;
-  }
-  return (
-    <div className="comparison-container">
-      <div className="card-container">{renderCard()}</div>
+  render() {
+    const { citiesData } = this.props;
+    const { getCityPop, getStateName } = this;
+    if (citiesData.length === 0) {
+      return <LoadingComponent message="Loading city data..." />;
+    }
+    return (
+      <div className="comparison-container">
+        <div className="card-container">
+          {citiesData.map(city => {
+            return (
+              <ComparisonCard
+                citiesData={city}
+                key={city.population.data.total_pop}
+              />
+            );
+          })}
+        </div>
 
-      {/* Renders the tabs for the user to navigate for different visuals */}
-      <Tabs
-        className="metrics-container"
-        defaultActiveKey="1"
-        onChange={callback}
-        centered="true"
-        tabBarStyle={{
-          color: "white"
-        }}
-      >
-        <TabPane className="graph-holder" tab="Population Trend" key="1">
-          <Graph
-            dataSet={getCityPop()[0]}
-            dataSet2={getCityPop()[1]}
-            dataSet3={getCityPop()[2]}
-          />
-        </TabPane>
-        <TabPane className="graph-holder" tab="Unemployment Rate" key="2">
-          <Graph
-            dataSet={getStateName()[0]}
-            dataSet2={getStateName()[1]}
-            dataSet3={getStateName()[2]}
-          />
-        </TabPane>
-        {/* Will implement  */}
-        {/* <TabPane className="graph-holder" tab="Example Pie" key="3">
-              <PieChart />
-            </TabPane> */}
-      </Tabs>
-      <ModalComponent visible={visible} setVisible={setVisible} city={city} />
-    </div>
-  );
+        {/* Renders the tabs for the user to navigate for different visuals */}
+        <Tabs
+          data-testid="ant-d-tabs"
+          className="metrics-container"
+          defaultActiveKey="1"
+          centered="true"
+          tabBarStyle={{
+            color: "white"
+          }}
+        >
+          <Tabs.TabPane className="graph-holder" tab="Population Trend" key="1">
+            <Graph
+              dataSet={getCityPop()[0]}
+              dataSet2={getCityPop()[1]}
+              dataSet3={getCityPop()[2]}
+            />
+          </Tabs.TabPane>
+          <Tabs.TabPane
+            className="graph-holder"
+            tab="Unemployment Rate"
+            key="2"
+          >
+            <Graph
+              dataSet={getStateName()[0]}
+              dataSet2={getStateName()[1]}
+              dataSet3={getStateName()[2]}
+            />
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
+    );
+  }
 }
+
+export default RenderComparison;

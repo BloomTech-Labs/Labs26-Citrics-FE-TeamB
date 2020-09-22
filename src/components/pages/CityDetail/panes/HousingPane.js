766658796de946a42198d309ae4b351047857755
David Horstman <davidzilla27@gmail.com>
2020-09-22 15:48:53 -0700

    Add explanatory comments

--- src/components/pages/CityDetail/panes/HousingPane.js
+++ src/components/pages/CityDetail/panes/HousingPane.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Tabs } from "antd";
 export default function HousingPane({ rent }) {
   const { TabPane } = Tabs;
+  // This list will be the titles of the tabs
+  // it also matches the keys in rent (although they're all lowercase in rent)
   const aptTypes = ["Studio", "1BR", "2BR", "3BR", "4BR"];
   return (
     <Tabs defaultActiveKey="0">

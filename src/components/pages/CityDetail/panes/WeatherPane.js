dda8c844936ddcc71650a3f317cff5f675d91298
Rachele Edwards <dot.com1472@gmail.com>
2020-09-23 11:30:27 -0600

    tried making carousel from antd to work--not working, sourcing other coded carousels

--- src/components/pages/CityDetail/panes/WeatherPane.js
+++ src/components/pages/CityDetail/panes/WeatherPane.js
@@ -1,5 +1,32 @@
 import React from "react";
+//Styling
+import { Carousel } from "antd";
+
+const contentStyle = {
+  height: "100px",
+  width: "100px",
+  color: "white",
+  lineHeight: "100px",
+  textAlign: "center",
+  background: "purple"
+};
 
 export default function WeatherPane({ weather }) {
-  return <> Weather: {weather.summer_maxtempF_mean}</>;
+  return (
+    <h1>Hello</h1>
+    // <Carousel autoplay>
+    //   <div>
+    //     <h1 style={contentStyle}>WE'RE OVER HERE</h1>
+    //   </div>
+    //   <div>
+    //     <h1 style={contentStyle}>WE'RE OVER HERE</h1>
+    //   </div>
+    //   <div>
+    //     <h1 style={contentStyle}>WE'RE OVER HERE</h1>
+    //   </div>
+    //   <div>
+    //     <h1 style={contentStyle}>WE'RE OVER HERE</h1>
+    //   </div>
+    // </Carousel>
+  );
 }

5891f86e61dc64be52a527e2aaf76450c81bdcc2
Rachele Edwards <dot.com1472@gmail.com>
2020-09-23 16:38:06 -0600

    laid out weather html to render, and added classnames for styling

--- src/components/pages/CityDetail/panes/WeatherPane.js
+++ src/components/pages/CityDetail/panes/WeatherPane.js
@@ -1,6 +1,28 @@
 import React from "react";
+import humidity from "../../../../styles/icons/humidity-96.png";
+import summer from "../../../../styles/icons/summer-96.png";
+import winter from "../../../../styles/icons/winter-96.png";
 
 export default function WeatherPane({ weather }) {
-  // return <>Weather: {weather.summer_maxtempF_mean}</>;
-  return <p>Weather: {weather.summer_maxtempF_mean}</p>;
+  return (
+    <div className="weather-pane-container">
+      <h4>Weather</h4>
+      <div className="weather-metric-parent">
+        <img className="weather-icon" src={summer} alt="Sun icon" />
+        <h6>Average High: {weather.summer_maxtempF_mean}°</h6>
+      </div>
+      <div className="weather-metric-parent">
+        <img className="weather-icon" src={winter} alt="Snowflake icon" />
+        <h6>Average Low: {weather.winter_mintempF_mean}°</h6>
+      </div>
+      <div className="weather-metric-parent">
+        <img
+          className="weather-icon"
+          src={humidity}
+          alt="Three vertical zigzag lines to represent humidity icon"
+        />
+        <h6>Humidity: {weather.summer_humidity_mean}%</h6>
+      </div>
+    </div>
+  );
 }

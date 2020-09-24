cd26d828ffe4589fbe52c311867883e3ba3557d6
Rachele Edwards <dot.com1472@gmail.com>
2020-09-24 16:46:38 -0600

    adjusted divs to have two columns for weather data--got rid of h4 title since weather seems apparent due to icons

--- src/components/pages/CityDetail/panes/WeatherPane.js
+++ src/components/pages/CityDetail/panes/WeatherPane.js
@@ -2,26 +2,38 @@ import React from "react";
 import humidity from "../../../../styles/icons/humidity-96.png";
 import summer from "../../../../styles/icons/summer-96.png";
 import winter from "../../../../styles/icons/winter-96.png";
+import feelsLike from "../../../../styles/icons/feelsLike-96.png";
 
 export default function WeatherPane({ weather, currentWeather }) {
   return (
     <div className="weather-pane-container">
-      <h4>Weather</h4>
-      <div className="weather-metric-parent">
-        <img className="weather-icon" src={summer} alt="Sun icon" />
-        <h6>Average High: {weather.summer_maxtempF_mean}°</h6>
+      <div className="first-box">
+        <div className="weather-metric-parent">
+          <img className="weather-icon" src={summer} alt="Sun icon" />
+          <h6>Average High: {weather.summer_maxtempF_mean}°</h6>
+        </div>
+        <div className="weather-metric-parent">
+          <img className="weather-icon" src={winter} alt="Snowflake icon" />
+          <h6>Average Low: {weather.winter_mintempF_mean}°</h6>
+        </div>
+        <div className="weather-metric-parent">
+          <img
+            className="weather-icon"
+            src={humidity}
+            alt="Three vertical zigzag lines to represent humidity icon"
+          />
+          <h6>Humidity: {currentWeather.current.humidity}%</h6>
+        </div>
       </div>
-      <div className="weather-metric-parent">
-        <img className="weather-icon" src={winter} alt="Snowflake icon" />
-        <h6>Average Low: {weather.winter_mintempF_mean}°</h6>
-      </div>
-      <div className="weather-metric-parent">
-        <img
-          className="weather-icon"
-          src={humidity}
-          alt="Three vertical zigzag lines to represent humidity icon"
-        />
-        <h6>Humidity: {currentWeather.current.humidity}%</h6>
+      <div className="second-box">
+        <div className="weather-metric-parent">
+          <img
+            className="weather-icon"
+            src={feelsLike}
+            alt="Thermometer icon"
+          />
+          <h6>Today feels like: {currentWeather.current.feels_like}°</h6>
+        </div>
       </div>
     </div>
   );

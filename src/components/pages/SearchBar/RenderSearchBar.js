40948891d2e0a631e4f30f0d81a4332b7e36257f
Rachele Edwards <dot.com1472@gmail.com>
2020-09-28 13:24:23 -0600

    started layout of new inputs for advanced search

--- src/components/pages/SearchBar/RenderSearchBar.js
+++ src/components/pages/SearchBar/RenderSearchBar.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { AutoComplete } from "antd";
+import {
+  AutoComplete,
+  Input,
+  Select,
+  InputNumber,
+  Steps,
+  Button,
+  message
+} from "antd";
 
 export default function RenderSearchBar({
   searchTerm,
@@ -17,6 +25,47 @@ export default function RenderSearchBar({
         onChange={onChange}
         placeholder="Enter City.."
       />
+      <br />
+      <br />
+
+      <div className="site-input-group-wrapper">
+        {/* For job industry--type and autocomplete */}
+        <Input.Group compact>
+          <label for="jobs">Job Industry:</label>
+          <br />
+          <Input style={{ width: "50%" }} placeholder="Ex: Tech" />
+        </Input.Group>
+
+        {/* For population and rent price */}
+        <br />
+        <Input.Group compact>
+          <label for="between">Population: </label>
+          <br />
+          <Input
+            style={{ width: 90, textAlign: "center" }}
+            placeholder="Minimum"
+          />
+          <Input
+            className="site-input-split"
+            style={{
+              width: 30,
+              borderLeft: 0,
+              borderRight: 0,
+              pointerEvents: "none"
+            }}
+            placeholder="-"
+            disabled
+          />
+          <Input
+            className="site-input-right"
+            style={{
+              width: 90,
+              textAlign: "center"
+            }}
+            placeholder="Maximum"
+          />
+        </Input.Group>
+      </div>
     </div>
   );
 }

358fccba47e92f56358975484f1a5187cf46f61c
Rachele Edwards <dot.com1472@gmail.com>
2020-10-01 12:13:17 -0600

    added icons to styles folder, population, weather, rent price. Left aligned the comparison metrics card and gave it padding--added padding to the icons

--- src/components/pages/Comparison/comparisonCard.js
+++ src/components/pages/Comparison/comparisonCard.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import ModalComponent from "../../common/Modal.js";
 import LoadingComponent from "../../common/LoadingComponent";
+//icons
+import weather from "../../../styles/icons/weather-48.png";
+import population from "../../../styles/icons/pop-48.png";
+import pricing from "../../../styles/icons/pricing-48.png";
 
 class ComparisonCard extends Component {
   state = {
@@ -33,16 +37,23 @@ class ComparisonCard extends Component {
             <LoadingComponent message="Loading city data..." />
           ) : (
             <div className="basic-card-info">
-              <h3>
-                {citiesData.name}, {citiesData.state}
-              </h3>
-              <p>
-                {`Population: ${citiesData.population.data.total_pop.toLocaleString()}`}
-              </p>
-              <p>{`Rent: ${"$" + citiesData.rent["1br"]}/month (1BR)`}</p>
-              <p>
-                {`Weather: ${citiesData.weather.winter_mintempF_mean}°F - ${citiesData.weather.summer_maxtempF_mean}°F`}
-              </p>
+              <div className="card-metrics">
+                <h3>
+                  {citiesData.name}, {citiesData.state}
+                </h3>
+                <p>
+                  <img src={population} alt="Population icon" />
+                  {citiesData.population.data.total_pop.toLocaleString()}
+                </p>
+                <p>
+                  <img src={pricing} alt="Rental pricing icon" />
+                  {`${"$" + citiesData.rent["1br"]}/month (1BR)`}
+                </p>
+                <p>
+                  <img src={weather} alt="Weather icon" />
+                  {`${citiesData.weather.winter_mintempF_mean}°F - ${citiesData.weather.summer_maxtempF_mean}°F`}
+                </p>
+              </div>
               <div className="btn-container">
                 <button
                   className="more-info-btn"

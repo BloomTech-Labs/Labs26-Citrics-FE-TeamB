d40dce35b00b1a035cf8f1bdda7ffeff7b33ec27
lyndsiWilliams <kcatgirl@gmail.com>
2020-10-01 16:10:15 -0500

    Added styling

--- src/components/pages/AboutPage/RenderAboutPage.js
+++ src/components/pages/AboutPage/RenderAboutPage.js
@@ -25,6 +25,21 @@ const RenderAboutPage = ({ isLoading }) => {
       )}
       */}
 
+      {/* ----- Introduction ----- */}
+      <section className="about-introduction">
+        <h1>The Citrics Team</h1>
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida
+          tincidunt euismod. Mauris lacus mi, vulputate eget sem vitae,
+          imperdiet consectetur urna. In ante sapien, mattis eu nisi ut,
+          malesuada fringilla massa. Ut pulvinar, quam quis interdum convallis,
+          sem dolor ullamcorper lacus, vel tristique felis nunc in risus. Duis
+          ex dolor, commodo vitae metus ac, egestas tincidunt turpis. Morbi leo
+          quam, ornare molestie pellentesque interdum, euismod ut magna. Aliquam
+          tincidunt orci vulputate interdum varius.
+        </h3>
+      </section>
+
       {/* ----- Team cards ----- */}
 
       <section className="about-tpl">

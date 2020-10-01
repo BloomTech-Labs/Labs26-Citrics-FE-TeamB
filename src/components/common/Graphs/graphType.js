f20a23c69447541893f2360fec9cf60d8321fb01
alanblee <alanbenlee12@gmail.com>
2020-09-30 17:31:58 -0700

    only using the pie chart for details

--- src/components/common/Graphs/graphType.js
+++ src/components/common/Graphs/graphType.js
@@ -2,6 +2,7 @@ export const lineGraph = (set1, set2, set3) => {
   let trace2, trace3;
 
   let trace1 = {
+    automargin: true,
     x: set1.plotX,
     y: set1.plotY,
     line: { color: "rgba(222,45,38,0.8)" },
@@ -11,6 +12,7 @@ export const lineGraph = (set1, set2, set3) => {
   };
   if (set2) {
     trace2 = {
+      automargin: true,
       x: set2.plotX,
       y: set2.plotY,
       line: { color: "rgb(49,130,189)" },
@@ -20,6 +22,7 @@ export const lineGraph = (set1, set2, set3) => {
   }
   if (set3) {
     trace3 = {
+      automargin: true,
       x: set3.plotX,
       y: set3.plotY,
       line: { color: "rgb(204,204,204)" },
@@ -32,19 +35,13 @@ export const lineGraph = (set1, set2, set3) => {
   let layout = {
     paper_bgcolor: "transparent",
     plot_bgcolor: "transparent",
-    // yaxis: {
-    //   showgrid: false
-    // },
-    // xaxis: {
-    //   showgrid: false
-    // },
     font: {
       size: 14,
       color: "#000"
     },
-
     showlegend: true,
-    title: set1.graphName
+    title: set1.graphName,
+    autosize: true
   };
   return {
     dataPlot,
@@ -56,6 +53,7 @@ export const barGraph = (set1, set2, set3) => {
   let trace2, trace3;
 
   let trace1 = {
+    automargin: true,
     x: set1.plotX,
     y: set1.plotY,
     type: "bar",
@@ -68,6 +66,7 @@ export const barGraph = (set1, set2, set3) => {
   };
   if (set2) {
     trace2 = {
+      automargin: true,
       x: set2.plotX,
       y: set2.plotY,
       type: "bar",
@@ -81,6 +80,7 @@ export const barGraph = (set1, set2, set3) => {
   }
   if (set3) {
     trace3 = {
+      automargin: true,
       x: set3.plotX,
       y: set3.plotY,
       type: "bar",
@@ -96,14 +96,9 @@ export const barGraph = (set1, set2, set3) => {
 
   let layout = {
     title: set1.graphName,
+    autosize: true,
     paper_bgcolor: "transparent",
     plot_bgcolor: "transparent",
-    // yaxis: {
-    //   showgrid: false
-    // },
-    // xaxis: {
-    //   showgrid: false
-    // },
     font: {
       size: 14,
       color: "#000"
@@ -114,93 +109,36 @@ export const barGraph = (set1, set2, set3) => {
 
   return { dataPlot, layout };
 };
-export const pieChart = (set1, set2, set3) => {
-  var ultimateColors = [
-    [
-      "rgb(56, 75, 126)",
-      "rgb(18, 36, 37)",
-      "rgb(34, 53, 101)",
-      "rgb(36, 55, 57)",
-      "rgb(6, 4, 4)"
-    ],
-    [
-      "rgb(177, 127, 38)",
-      "rgb(205, 152, 36)",
-      "rgb(99, 79, 37)",
-      "rgb(129, 180, 179)",
-      "rgb(124, 103, 37)"
-    ],
-    [
-      "rgb(33, 75, 99)",
-      "rgb(79, 129, 102)",
-      "rgb(151, 179, 100)",
-      "rgb(175, 49, 35)",
-      "rgb(36, 73, 147)"
-    ],
-    [
-      "rgb(146, 123, 21)",
-      "rgb(177, 180, 34)",
-      "rgb(206, 206, 40)",
-      "rgb(175, 51, 21)",
-      "rgb(35, 36, 21)"
-    ]
-  ];
+export const pieChart = set1 => {
   let pieData = [
     {
       values: set1.values,
       labels: set1.labels,
       type: "pie",
       name: set1.name,
-      marker: {
-        colors: ultimateColors[0]
-      },
-      domain: {
-        row: 0,
-        column: 0
-      },
+      // marker: {
+      //   colors: ultimateColors[0],
+      // },
       hoverinfo: "label+percent+name",
-      textinfo: "none"
-    },
-    set2
-      ? {
-          values: set2.values,
-          labels: set2.labels,
-          type: "pie",
-          name: set2.name,
-          marker: {
-            colors: ultimateColors[1]
-          },
-          domain: {
-            row: 1,
-            column: 0
-          },
-          hoverinfo: "label+percent+name",
-          textinfo: "none"
-        }
-      : {},
-    set3
-      ? {
-          values: set3.values,
-          labels: set3.labels,
-          type: "pie",
-          name: set3.name,
-          marker: {
-            colors: ultimateColors[2]
-          },
-          domain: {
-            row: 0,
-            column: 1
-          },
-          hoverinfo: "label+percent+name",
-          textinfo: "none"
-        }
-      : {}
+      textinfo: "none",
+      automargin: true
+    }
   ];
 
   let layout = {
-    height: 1000,
-    width: 1000,
-    grid: { rows: 1, columns: 2 }
+    title: "Job Market",
+    showlegend: false,
+    paper_bgcolor: "transparent",
+    plot_bgcolor: "transparent",
+    yaxis: {
+      showgrid: false
+    },
+    xaxis: {
+      showgrid: false
+    },
+    autosize: true,
+    width: 300
   };
+
   return { pieData, layout };
 };

d9724771899938909df82ebf2d853622de6f4512
David Horstman <davidzilla27@gmail.com>
2020-10-05 12:49:50 -0700

    Add skeleton to titlebar in CityDetail

--- src/components/pages/CityDetail/RenderCityDetail.js
+++ src/components/pages/CityDetail/RenderCityDetail.js
@@ -1,22 +1,28 @@
+import { Skeleton } from "antd";
 import React from "react";
 import { HousingPane, JobsPane, PopulationPane, WeatherPane } from "./panes";
 export default function RenderCityDetail({ city }) {
   return (
     <div className="city-detail-card">
       <h3 className="one-render-h3">
-        {city.name}, {city.state}
+        {city?.name ? (
+          `${city.name}, ${city.state}`
+        ) : (
+          <Skeleton active title={{ width: "30%" }} paragraph={false} />
+        )}
       </h3>
 
-      <PopulationPane population={city.population} />
-
-      <WeatherPane
-        weather={city.weather}
-        currentWeather={city.currentWeather}
-      />
-
-      <HousingPane rent={city.rent} />
-
-      <JobsPane jobs={city.jobs} />
+      {city?.weather && (
+        <>
+          <PopulationPane population={city.population} />
+          <WeatherPane
+            weather={city.weather}
+            currentWeather={city.currentWeather}
+          />
+          <HousingPane rent={city.rent} />
+          <JobsPane jobs={city.jobs} />
+        </>
+      )}
     </div>
   );
 }

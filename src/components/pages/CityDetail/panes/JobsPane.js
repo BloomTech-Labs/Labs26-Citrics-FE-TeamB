6c09ff4813f47aa5b557c2094c7ba9190e96ac56
alanblee <alanbenlee12@gmail.com>
2020-10-05 15:37:48 -0700

    removed tags, redundent with the pie chart legend

--- src/components/pages/CityDetail/panes/JobsPane.js
+++ src/components/pages/CityDetail/panes/JobsPane.js
@@ -1,54 +1,10 @@
 import React from "react";
 import { CarOutlined } from "@ant-design/icons";
-import { Tag } from "antd";
+
 import Plot from "react-plotly.js";
 
 export default function JobsPane({ jobs }) {
   console.log(jobs, "job industry");
-
-  // function that renders top 10 job industries
-  const renderJobTags = () => {
-    // helper function to build keys to filter
-    const helperFunc = (str = "job_ranked_") => {
-      const allowed = [];
-      for (let i = 1; i < 11; i++) {
-        allowed.push(`${str}${i}`);
-      }
-      return allowed;
-    };
-    const filterdJobKeys = helperFunc();
-    // filters the job keys creating an array of the values for given key
-    const topJobArray = Object.keys(jobs.data)
-      .filter(keys => filterdJobKeys.includes(keys))
-      .map(jobValue => {
-        return jobs.data[jobValue];
-      });
-
-    // color array to randomly choose from everytime a tag component is created
-    const colors = [
-      "magenta",
-      "red",
-      "volcano",
-      "orange",
-      "gold",
-      "lime",
-      "green",
-      "cyan",
-      "blue",
-      "geekblue",
-      "purple"
-    ];
-    return topJobArray.map((jobName, indx) => {
-      return (
-        <Tag
-          color={colors[Math.floor(Math.random() * colors.length)]}
-          key={indx}
-        >
-          {jobName}
-        </Tag>
-      );
-    });
-  };
 
   // function that parses and renders the given pie chart
   const renderPie = () => {
@@ -63,8 +19,7 @@ export default function JobsPane({ jobs }) {
       }
     ];
     const layout = {
-      title: "Top Industry",
-      showlegend: false,
+      showlegend: true,
       paper_bgcolor: "transparent",
       plot_bgcolor: "transparent",
       yaxis: {
@@ -93,7 +48,6 @@ export default function JobsPane({ jobs }) {
         </div>
         <div className="job-info-container">
           <h3>Top 10 Industries</h3>
-          <div className="job-tags">{renderJobTags()}</div>
           <div className="job-charts">{renderPie()}</div>
         </div>
       </div>

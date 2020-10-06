87c054c969c1765b2496dfc64a6854182cebcb39
Rachele Edwards <dot.com1472@gmail.com>
2020-10-06 15:19:51 -0600

    cleanup deleting the ternary, moving the useEffect and functions to toggle. Moved logic into NavContainer

--- src/components/pages/Nav/RenderNav.js
+++ src/components/pages/Nav/RenderNav.js
@@ -1,5 +1,5 @@
 // Library imports
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Drawer, Button } from "antd";
 import { DoubleLeftOutlined, DoubleRightOutlined } from "@ant-design/icons";
 
@@ -13,89 +13,34 @@ import drawerWidth from "./drawerWidth";
 
 export default function RenderNav({ toggleDrawer, isOpen }) {
   const buttonTransform = isOpen ? `translate(${drawerWidth}px,0px)` : "";
-  //for media query (see ternary below)--allows floating button to slide in and out with the nav
-  const reverseButtonTransform = !isOpen
-    ? `translate(${drawerWidth}px,0px)`
-    : "";
-
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    function handleResize() {
-      setWidth(window.innerWidth);
-    }
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [width]);
-
-  useEffect(() => {
-    width < 1000 && handleSideNavToggle();
-  }, [width]);
-
-  function handleSideNavToggle() {
-    console.log("toggle it");
-  }
 
   return (
     <div className="navbar">
-      {/* media query--changing the toggle to be shut if screen sidth is 1000px or less */}
-      {width > 1000 ? (
-        <>
-          <Button
-            className="floating-visibility-button"
-            data-testid="floating-visibility-button"
-            type="secondary"
-            onClick={toggleDrawer}
-            style={{ transform: buttonTransform }}
-          >
-            {isOpen ? <DoubleLeftOutlined /> : <DoubleRightOutlined />}
-          </Button>
-          <Drawer
-            title="City Search"
-            placement="left"
-            closable={false}
-            onClose={toggleDrawer}
-            visible={isOpen}
-            mask={false}
-            width={drawerWidth}
-          >
-            <SearchBar />
-            <br />
-            <br />
-            <br />
+      <Button
+        className="floating-visibility-button"
+        data-testid="floating-visibility-button"
+        type="secondary"
+        onClick={toggleDrawer}
+        style={{ transform: buttonTransform }}
+      >
+        {isOpen ? <DoubleLeftOutlined /> : <DoubleRightOutlined />}
+      </Button>
+      <Drawer
+        title="City Search"
+        placement="left"
+        closable={false}
+        onClose={toggleDrawer}
+        visible={isOpen}
+        mask={false}
+        width={drawerWidth}
+      >
+        <SearchBar />
+        <br />
+        <br />
+        <br />
 
-            <SelectedCities />
-          </Drawer>
-        </>
-      ) : (
-        <>
-          <Button
-            className="floating-visibility-button"
-            data-testid="floating-visibility-button"
-            type="secondary"
-            onClick={toggleDrawer}
-            style={{ transform: reverseButtonTransform }}
-          >
-            {!isOpen ? <DoubleLeftOutlined /> : <DoubleRightOutlined />}
-          </Button>
-          <Drawer
-            title="City Search"
-            placement="left"
-            closable={false}
-            onClose={toggleDrawer}
-            visible={!isOpen}
-            mask={false}
-            width={drawerWidth}
-          >
-            <SearchBar />
-            <br />
-            <br />
-            <br />
-
-            <SelectedCities />
-          </Drawer>
-        </>
-      )}
+        <SelectedCities />
+      </Drawer>
     </div>
   );
 }

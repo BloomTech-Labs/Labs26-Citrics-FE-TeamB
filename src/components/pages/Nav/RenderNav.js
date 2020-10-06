6f02c336d46d25127584d6197128c8c9e13c8a87
Rachele Edwards <dot.com1472@gmail.com>
2020-10-06 11:30:44 -0600

    changed the rendering to be able to have media query--any screen width 1000px and less will automatically close the nav bar, and can only be opened with floating button. Added useEffect, variable for reversing float button, and ternary for rendering depending of screen width

--- src/components/pages/Nav/RenderNav.js
+++ src/components/pages/Nav/RenderNav.js
@@ -1,5 +1,6 @@
 // Library imports
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import { Drawer, Button } from "antd";
 import { DoubleLeftOutlined, DoubleRightOutlined } from "@ant-design/icons";
 
@@ -13,34 +14,89 @@ import drawerWidth from "./drawerWidth";
 
 export default function RenderNav({ toggleDrawer, isOpen }) {
   const buttonTransform = isOpen ? `translate(${drawerWidth}px,0px)` : "";
+  //to be able to move button in right direction when reaches 1000px screen width (see render below)
+  const reverseButtonTransform = !isOpen
+    ? `translate(${drawerWidth}px,0px)`
+    : "";
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width]);
+
+  useEffect(() => {
+    width < 600 && handleSideNavToggle();
+  }, [width]);
+
+  function handleSideNavToggle() {
+    console.log("toggle it");
+  }
 
   return (
     <div className="navbar">
-      <Button
-        className="floating-visibility-button"
-        data-testid="floating-visibility-button"
-        type="secondary"
-        onClick={toggleDrawer}
-        style={{ transform: buttonTransform }}
-      >
-        {isOpen ? <DoubleLeftOutlined /> : <DoubleRightOutlined />}
-      </Button>
-      <Drawer
-        title="City Search"
-        placement="left"
-        closable={false}
-        onClose={toggleDrawer}
-        visible={isOpen}
-        mask={false}
-        width={drawerWidth}
-      >
-        <SearchBar />
-        <br />
-        <br />
-        <br />
+      {/* media query--changing the toggle to be shut if screen sidth is 1000px or less */}
+      {width > 1000 ? (
+        <>
+          <Button
+            className="floating-visibility-button"
+            data-testid="floating-visibility-button"
+            type="secondary"
+            onClick={toggleDrawer}
+            style={{ transform: buttonTransform }}
+          >
+            {isOpen ? <DoubleLeftOutlined /> : <DoubleRightOutlined />}
+          </Button>
+          <Drawer
+            title="City Search"
+            placement="left"
+            closable={false}
+            onClose={toggleDrawer}
+            visible={isOpen}
+            mask={false}
+            width={drawerWidth}
+          >
+            <SearchBar />
+            <br />
+            <br />
+            <br />
 
-        <SelectedCities />
-      </Drawer>
+            <SelectedCities />
+          </Drawer>
+        </>
+      ) : (
+        <>
+          <Button
+            className="floating-visibility-button"
+            data-testid="floating-visibility-button"
+            type="secondary"
+            onClick={toggleDrawer}
+            style={{ transform: reverseButtonTransform }}
+          >
+            {!isOpen ? <DoubleLeftOutlined /> : <DoubleRightOutlined />}
+          </Button>
+          <Drawer
+            title="City Search"
+            placement="left"
+            closable={false}
+            onClose={toggleDrawer}
+            visible={!isOpen}
+            mask={false}
+            width={drawerWidth}
+          >
+            <SearchBar />
+            <br />
+            <br />
+            <br />
+
+            <SelectedCities />
+          </Drawer>
+        </>
+      )}
     </div>
   );
 }

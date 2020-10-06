be7c502e5f6515814c351680e516dfff222553d6
alanblee <alanbenlee12@gmail.com>
2020-10-05 18:23:56 -0700

    added div to contain weather and rentail

--- src/components/pages/CityDetail/RenderCityDetail.js
+++ src/components/pages/CityDetail/RenderCityDetail.js
@@ -6,11 +6,13 @@ export default function RenderCityDetail({ city }) {
       <h3 className="one-render-h3">
         {city.name}, {city.state}
       </h3>
-      <WeatherPane
-        weather={city.weather}
-        currentWeather={city.currentWeather}
-      />
-      <HousingPane rent={city.rent} />
+      <div className="pane-container">
+        <WeatherPane
+          weather={city.weather}
+          currentWeather={city.currentWeather}
+        />
+        <HousingPane rent={city.rent} />
+      </div>
       <JobsPane jobs={city.jobs} unemployment={city.unemployRate} />
       <PopulationPane population={city.population} />
     </div>

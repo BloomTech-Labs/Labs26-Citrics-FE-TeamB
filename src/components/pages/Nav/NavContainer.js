6fa11170086973b66835d7c3ab0ded9d832c01de
Rachele Edwards <dot.com1472@gmail.com>
2020-10-06 15:21:32 -0600

    cleanup

--- src/components/pages/Nav/NavContainer.js
+++ src/components/pages/Nav/NavContainer.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { toggleDrawer } from "../../../state/actions";
 
 import RenderNav from "./RenderNav";
 
 function NavContainer({ isOpen, toggleDrawer }) {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width]);
+
+  useEffect(() => {
+    width < 1000 && toggleDrawer();
+  }, [width, toggleDrawer]);
+
   return <RenderNav isOpen={isOpen} toggleDrawer={toggleDrawer} />;
 }
 const mapPropsToState = ({ drawer: { isOpen } }, props) => ({

6c49bd6f790b9442310ec0f6d050b3234f021b11
lyndsiWilliams <kcatgirl@gmail.com>
2020-10-07 17:49:22 -0500

    Added LinkedIn descriptions to card modals

--- src/components/pages/AboutPage/RenderAboutPage.js
+++ src/components/pages/AboutPage/RenderAboutPage.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 // About page
 // import AboutPageContainer from "./AboutPageContainer";
 // Styling
-import { Card, Avatar, Modal, Button } from "antd";
+import { Card, Avatar, Modal, Button, Divider } from "antd";
 import {
   GithubOutlined,
   MailOutlined,
@@ -19,11 +19,13 @@ const { Meta } = Card;
 
 const RenderAboutPage = ({ isLoading }) => {
   // Modal state
-  const [modalVisibility, setModalVisibility] = useState(false);
-
-  const setModalVisible = modalVisibility => {
-    setModalVisibility(modalVisibility);
-  };
+  const [bhavaniModalVisibility, setBhavaniModalVisibility] = useState(false);
+  const [ekramModalVisibility, setEkramModalVisibility] = useState(false);
+  const [zackModalVisibility, setZackModalVisibility] = useState(false);
+  const [alanModalVisibility, setAlanModalVisibility] = useState(false);
+  const [davidModalVisibility, setDavidModalVisibility] = useState(false);
+  const [lyndsiModalVisibility, setLyndsiModalVisibility] = useState(false);
+  const [racheleModalVisibility, setRacheleModalVisibility] = useState(false);
 
   return (
     <div className="about-container">
@@ -87,20 +89,41 @@ const RenderAboutPage = ({ isLoading }) => {
           <Button
             className="modal-button"
             type="primary"
-            onClick={() => setModalVisible(true)}
+            onClick={() => setBhavaniModalVisibility(true)}
           >
             About me
           </Button>
           <Modal
             title="About me"
             centered
-            visible={modalVisibility}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
+            visible={bhavaniModalVisibility}
+            onOk={() => setBhavaniModalVisibility(false)}
+            onCancel={() => setBhavaniModalVisibility(false)}
           >
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
+            <p>
+              Data Scientist with a background is as a Java Programmer with 3
+              years of corporate experience. I studied at Lambda School,
+              currently working as a Technical Project Lead at Lambda School to
+              support the learning and professional growth of other Lambda
+              students while deepening my skills in Data Science. My skill set
+              includes Data Analysis, Machine Learning Engineering, Predictive
+              Analysis, Recommendation Systems, and Data Engineering. I'm
+              excited to take on a new challenge in the Data Science universe.
+            </p>
+            <Divider />
+            <p>Technical Skills</p>
+            <p>
+              Programming Expertise: ​Python (NumPy, Pandas,
+              Scikit-learn,Matplotlib, Seaborn), R, Java, SQL, PostgreSQL,
+              MongoDb, AS/400, DB2/400 and Excel
+            </p>
+            <p>Frameworks: ​TensorFlow, Keras, Flask, Fast API</p>
+            <p>
+              Skills​: Data analysis, Predictive analytics, Linear and
+              multivariate regressions,, K-cluster analysis, Classification,
+              Machine learning products, Natural language processing, Neural
+              Networks, Github, Amazon EBS, Heroku
+            </p>
           </Modal>
         </Card>
 
@@ -146,20 +169,27 @@ const RenderAboutPage = ({ isLoading }) => {
           <Button
             className="modal-button"
             type="primary"
-            onClick={() => setModalVisible(true)}
+            onClick={() => setEkramModalVisibility(true)}
           >
             About me
           </Button>
           <Modal
             title="About me"
             centered
-            visible={modalVisibility}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
+            visible={ekramModalVisibility}
+            onOk={() => setEkramModalVisibility(false)}
+            onCancel={() => setEkramModalVisibility(false)}
           >
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
+            <p>
+              With a background in Maritime science and Marine engineering, and
+              being blessed of having the opportunity of working onboard
+              ocean-going vessel, I and eager to utilize my knowledge and
+              experience towards my new career of Data Science. I want to learn
+              new technical skills in data science, enhance my knowledge in
+              ocean & marine science and keep educating myself in order to
+              fulfill my ultimate goal, which is to have a humble contribution
+              towards ocean sustainability.
+            </p>
           </Modal>
         </Card>
 
@@ -197,20 +227,26 @@ const RenderAboutPage = ({ isLoading }) => {
           <Button
             className="modal-button"
             type="primary"
-            onClick={() => setModalVisible(true)}
+            onClick={() => setZackModalVisibility(true)}
           >
             About me
           </Button>
           <Modal
             title="About me"
             centered
-            visible={modalVisibility}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
+            visible={zackModalVisibility}
+            onOk={() => setZackModalVisibility(false)}
+            onCancel={() => setZackModalVisibility(false)}
           >
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
+            <p>
+              Data scientist with a passion for solving problems and exploring
+              algorithms. Friendship ended with SQL, now machine learning is my
+              best friend. Just kidding, I still love you SQL. Have a background
+              in business management and customer service, both are very
+              important to me and have been instrumental in molding the
+              programmer I am today. Physically present in CT, but my heart
+              still resides in CO.
+            </p>
           </Modal>
         </Card>
       </section>
@@ -267,20 +303,23 @@ const RenderAboutPage = ({ isLoading }) => {
           <Button
             className="modal-button"
             type="primary"
-            onClick={() => setModalVisible(true)}
+            onClick={() => setAlanModalVisibility(true)}
           >
             About me
           </Button>
           <Modal
             title="About me"
             centered
-            visible={modalVisibility}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
+            visible={alanModalVisibility}
+            onOk={() => setAlanModalVisibility(false)}
+            onCancel={() => setAlanModalVisibility(false)}
           >
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
+            <p>
+              Motivated developer with experience creating custom websites.
+              Strong collaboration skills required to deliver projects on time
+              while working with diverse remote teams. Outside of the code, I
+              enjoy being out in nature hiking and rock climbing.
+            </p>
           </Modal>
         </Card>
 
@@ -318,20 +357,24 @@ const RenderAboutPage = ({ isLoading }) => {
           <Button
             className="modal-button"
             type="primary"
-            onClick={() => setModalVisible(true)}
+            onClick={() => setDavidModalVisibility(true)}
           >
             About me
           </Button>
           <Modal
             title="About me"
             centered
-            visible={modalVisibility}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
+            visible={davidModalVisibility}
+            onOk={() => setDavidModalVisibility(false)}
+            onCancel={() => setDavidModalVisibility(false)}
           >
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
+            <p>
+              Software Engineer with strong programming skills and broad
+              experience, currently specialized in Web Development. Background
+              in computer science education and research science. Recent Lambda
+              School graduate with an MA in Chemistry from Columbia and a BS in
+              Biochemistry from UCSD.
+            </p>
           </Modal>
         </Card>
 
@@ -385,20 +428,24 @@ const RenderAboutPage = ({ isLoading }) => {
           <Button
             className="modal-button"
             type="primary"
-            onClick={() => setModalVisible(true)}
+            onClick={() => setLyndsiModalVisibility(true)}
           >
             About me
           </Button>
           <Modal
             title="About me"
             centered
-            visible={modalVisibility}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
+            visible={lyndsiModalVisibility}
+            onOk={() => setLyndsiModalVisibility(false)}
+            onCancel={() => setLyndsiModalVisibility(false)}
           >
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
+            <p>
+              I am a full-stack web developer with a strong love for Front End.
+              My passion for programming shows in my late-night adventures
+              getting lost in my latest project. I ensure sustainable working
+              practices by investing in breaks that typically involve video
+              games. The Pomodoro technique will save your life in this career!
+            </p>
           </Modal>
         </Card>
 
@@ -451,16 +498,16 @@ const RenderAboutPage = ({ isLoading }) => {
           <Button
             className="modal-button"
             type="primary"
-            onClick={() => setModalVisible(true)}
+            onClick={() => setRacheleModalVisibility(true)}
           >
             About me
           </Button>
           <Modal
             title="About me"
             centered
-            visible={modalVisibility}
-            onOk={() => setModalVisible(false)}
-            onCancel={() => setModalVisible(false)}
+            visible={racheleModalVisibility}
+            onOk={() => setRacheleModalVisibility(false)}
+            onCancel={() => setRacheleModalVisibility(false)}
           >
             <p>
               Hello, I’m Rachele! I love building functional apps and being able
@@ -472,6 +519,7 @@ const RenderAboutPage = ({ isLoading }) => {
               successful company derives from a team of positive, caring, and
               constructive employees.
             </p>
+            <Divider />
             <p>
               Fun fact about Rachele: “Not a daredevil, but love adventure! I
               enjoy many outdoor activities with family such as dirt-biking,

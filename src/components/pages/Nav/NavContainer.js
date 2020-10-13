4cc205a616107dc33b7ad1c7140dd1c31291a273
David Horstman <davidzilla27@gmail.com>
2020-10-12 18:05:22 -0700

    Use openDrawer and closeDrawer in NavContainer

--- src/components/pages/Nav/NavContainer.js
+++ src/components/pages/Nav/NavContainer.js
@@ -1,26 +1,29 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { toggleDrawer } from "../../../state/actions";
+import { openDrawer, closeDrawer, toggleDrawer } from "../../../state/actions";
 
 import RenderNav from "./RenderNav";
 
-function NavContainer({ isOpen, toggleDrawer }) {
+function NavContainer({ isOpen, openDrawer, closeDrawer, toggleDrawer }) {
   useEffect(() => {
     let width = window.innerWidth;
     // On load, if width is less than 1000, close drawer
-    if (width < 1000) toggleDrawer();
+    if (width < 1000) closeDrawer();
 
     // Event handler for window resizing
     function handleResize() {
-      // If we crossed the 100px threshold
-      if ((width < 1000) ^ (window.innerWidth < 1000)) {
-        toggleDrawer();
-        width = window.innerWidth;
+      // Open if we went from mobile view to desktop view
+      if (width < 1000 && window.innerWidth > 1000) {
+        openDrawer();
+        // Close if we went from desktop view to mobile view
+      } else if (width > 1000 && window.innerWidth < 1000) {
+        closeDrawer();
       }
+      width = window.innerWidth;
     }
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, [toggleDrawer]);
+  }, [openDrawer, closeDrawer]);
 
   return <RenderNav isOpen={isOpen} toggleDrawer={toggleDrawer} />;
 }
@@ -28,4 +31,8 @@ const mapPropsToState = ({ drawer: { isOpen } }, props) => ({
   isOpen,
   ...props
 });
-export default connect(mapPropsToState, { toggleDrawer })(NavContainer);
+export default connect(mapPropsToState, {
+  openDrawer,
+  closeDrawer,
+  toggleDrawer
+})(NavContainer);

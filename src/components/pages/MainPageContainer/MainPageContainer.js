309370c3df0736ebbf4d0e9454dcb54d82f56bfd
alanblee <alanbenlee12@gmail.com>
2020-10-14 14:10:07 -0700

    only adds width when width is > 1000

--- src/components/pages/MainPageContainer/MainPageContainer.js
+++ src/components/pages/MainPageContainer/MainPageContainer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import drawerWidth from "../Nav/drawerWidth";
+
 export const baseMargin = 0;
 function MainPageContainer({ children, isOpen }) {
-  const marginLeft = isOpen
-    ? `${drawerWidth + baseMargin}px`
-    : `${baseMargin}px`;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // timeoutId for debounce mechanism
+    let timeoutId = null;
+    function handleResize() {
+      // prevent execution of previous setTimeout
+      clearTimeout(timeoutId);
+      // change width from the state object after 500 milliseconds
+      timeoutId = setTimeout(() => setWidth(window.innerWidth), 500);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width]);
+  const marginLeft =
+    isOpen && width > 1000
+      ? `${drawerWidth + baseMargin}px`
+      : `${baseMargin}px`;
   return (
     <div data-testid="main-page-container" style={{ marginLeft }}>
       {children}

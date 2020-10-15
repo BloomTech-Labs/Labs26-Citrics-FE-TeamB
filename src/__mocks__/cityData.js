2c74c8451b79a08e8bfa6d4f8de0a9a00f4214c0
alanblee <alanbenlee12@gmail.com>
2020-10-15 14:38:03 -0700

    created mock city data file to use in both comparison and details tests

---
+++ src/__mocks__/cityData.js
@@ -0,0 +1,50 @@
+export const mockCityDetails = [
+  {
+    image: "https://i.imgur.com/YXdssOR.jpeg",
+    name: "Baton Rouge",
+    state: "LA",
+    population: {
+      data: {
+        total_pop: 221606
+      },
+      viz: JSON.stringify({
+        data: [{ x: [3, 5, 6, 8, 9] }, { y: [10, 2, 5, 21] }]
+      })
+    },
+    unemployRate: {
+      viz: JSON.stringify({
+        data: [{ x: [3, 5, 6, 8, 9] }, { y: [10, 2, 5, 21] }]
+      })
+    },
+    rent: { studio: 3412 },
+    weather: {
+      summer_humidity_mean: 77,
+      summer_maxtempF_mean: 91,
+      winter_mintempF_mean: 47
+    }
+  },
+  {
+    image: "https://i.imgur.com/YXdssOR.jpeg",
+    name: "Sacramento",
+    state: "CA",
+    population: {
+      data: {
+        total_pop: 22160216
+      },
+      viz: JSON.stringify({
+        data: [{ x: [3, 5, 6, 8, 9] }, { y: [10, 2, 5, 21] }]
+      })
+    },
+    unemployRate: {
+      viz: JSON.stringify({
+        data: [{ x: [3, 5, 6, 8, 9] }, { y: [10, 2, 5, 21] }]
+      })
+    },
+    rent: { studio: 3412 },
+    weather: {
+      summer_humidity_mean: 77,
+      summer_maxtempF_mean: 91,
+      winter_mintempF_mean: 47
+    }
+  }
+];

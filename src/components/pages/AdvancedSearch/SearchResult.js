f98c198abe45a0f47945c8ef19a27c7f090492bd
Rachele Edwards <dot.com1472@gmail.com>
2020-10-15 12:35:06 -0600

    ading styles to advanced search. Trying to figure out how to add text when hovering over plus button

--- src/components/pages/AdvancedSearch/SearchResult.js
+++ src/components/pages/AdvancedSearch/SearchResult.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { Button } from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
 
 import { connect } from "react-redux";
 import { addCity } from "../../../state/actions";
+
+// function mouseEnter() {
+//   document.text('Add to Comparison')
+// }
+// $(document).ready(function() {
+//   $('visible-when-hovers').hover(mouseEnter)
+// })
 
 function SearchResult({ id, name, state, addCity }) {
   const addCityToComparison = () => addCity({ id, name, state });
   return (
     <div className="search-result">
       {`${name}, ${state}`}&nbsp;&nbsp;
-      <Button onClick={addCityToComparison}>Add to Comparison</Button>
+      <div className="adding-container">
+        <PlusCircleOutlined
+          onClick={addCityToComparison}
+          className="add-to-compare-btn"
+        />
+        <p className="visible-when-hovers">Add to Comparison</p>
+      </div>
     </div>
   );
 }

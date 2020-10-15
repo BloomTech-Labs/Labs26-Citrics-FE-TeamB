843b8f2d27dd3e0d78d06f668f115611f4308d06
Rachele Edwards <dot.com1472@gmail.com>
2020-10-15 12:34:31 -0600

    fixed typo to complie correctly

--- src/components/pages/Comparison/RenderComparison.js
+++ src/components/pages/Comparison/RenderComparison.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ComparisonCard from "./ComparisonCard";
+import ComparisonCard from "./comparisonCard";
 import ComparisonGraphs from "./ComparisonGraphs";
 
 export default function RenderComparison({ citiesData }) {

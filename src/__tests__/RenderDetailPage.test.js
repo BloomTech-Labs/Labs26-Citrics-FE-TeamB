bd2da66934f69a9c2d87726dc823e2b4a0ae74ae
alanblee <alanbenlee12@gmail.com>
2020-10-15 16:24:43 -0700

    renders the dom element properly

--- src/__tests__/RenderDetailPage.test.js
+++ src/__tests__/RenderDetailPage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render, fireEvent, waitFor, cleanup } from "@testing-library/react";
+import { render, waitFor, cleanup } from "@testing-library/react";
 import { mockCityDetails } from "../__mocks__/cityData";
 import RenderCityDetail from "../components/pages/CityDetail/RenderCityDetail";
 
@@ -10,8 +10,21 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-// Renders the comparison page
 it("Renders the comparison without errors", () => {
   const div = document.createElement("div");
   ReactDOM.render(<RenderCityDetail city={mockCityDetails} />, div);
+});
+let component;
+describe("render city details test suite", () => {
+  beforeEach(() => {
+    component = render(<RenderCityDetail city={mockCityDetails} />);
+  });
+
+  it("renders the city detail to the page", async () => {
+    const { findAllByTestId } = component;
+    const detailImage = await waitFor(() =>
+      findAllByTestId("city-detail-card")
+    );
+    expect(detailImage[0]).toBeInTheDocument();
+  });
 });

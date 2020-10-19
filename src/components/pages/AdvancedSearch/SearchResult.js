2e9772d97a16425d7e8389f7f9bc4bf9754a1df5
David Horstman <davidzilla27@gmail.com>
2020-10-19 15:48:21 -0700

    Clean up and add comments

--- src/components/pages/AdvancedSearch/SearchResult.js
+++ src/components/pages/AdvancedSearch/SearchResult.js
@@ -6,39 +6,39 @@ import { addCity, getCityDetails } from "../../../state/actions";
 import CityDetail from "../../common/Modal";
 import { Button } from "antd";
 
-function SearchResult({
-  id,
-  name,
-  state,
-  addCity,
-  cityDetails,
-  getCityDetails
-}) {
+function SearchResult(props) {
+  const { id, name, state, addCity, cityDetails, getCityDetails } = props;
   const addCityToComparison = () => addCity({ id, name, state });
+  // Modal visibility
   const [visible, setVisible] = useState(false);
+
+  // Retrieve details if we open a modal and don't have those details
   useEffect(() => {
     if (visible && !cityDetails[id]) {
       getCityDetails({ id, name, state });
     }
   }, [id, name, state, visible, cityDetails, getCityDetails]);
   return (
-    <div className="search-result">
-      {`${name}, ${state}`}&nbsp;&nbsp;
-      <img
-        src={addIcon}
-        alt="circle with plus sign in middle icon"
-        onClick={addCityToComparison}
-        onMouseOver={e => (e.currentTarget.src = addIconHover)}
-        onMouseOut={e => (e.currentTarget.src = addIcon)}
-        className="add-to-compare-btn"
-      />
-      <Button onClick={() => setVisible(true)}>Details</Button>
+    <>
+      <div className="search-result">
+        {`${name}, ${state}`}&nbsp;&nbsp;
+        <img
+          src={addIcon}
+          alt="circle with plus sign in middle icon"
+          onClick={addCityToComparison}
+          onMouseOver={e => (e.currentTarget.src = addIconHover)}
+          onMouseOut={e => (e.currentTarget.src = addIcon)}
+          className="add-to-compare-btn"
+        />
+        <Button onClick={() => setVisible(true)}>Details</Button>
+      </div>
+      {/* Modal to show city details */}
       <CityDetail
         city={cityDetails[id] ?? { id, name, state }}
         visible={visible}
         toggleModal={() => setVisible(!visible)}
       />
-    </div>
+    </>
   );
 }
 const mapProps = ({ cities: { cityDetails } }, props) => ({

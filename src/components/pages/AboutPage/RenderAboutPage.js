bbf33734883e9c97cef26d06ce9dd48306eb659d
David Horstman <davidzilla27@gmail.com>
2020-10-20 11:45:47 -0700

    Create ProfileCard component

--- src/components/pages/AboutPage/RenderAboutPage.js
+++ src/components/pages/AboutPage/RenderAboutPage.js
@@ -2,14 +2,16 @@
 import React, { useState } from "react";
 // Styling
 import { Card, Avatar, Modal, Button, Divider } from "antd";
+
 import {
   GithubOutlined,
   MailOutlined,
   LinkedinOutlined
 } from "@ant-design/icons";
-import projectLead from "../../../styles/icons/projectLead.jpg";
 import dataScience from "../../../styles/icons/dataScience.png";
 import webDev from "../../../styles/icons/webDev.png";
+
+import ProfileCard from "./ProfileCard";
 
 const { Meta } = Card;
 
@@ -63,124 +65,7 @@ const RenderAboutPage = ({ returnToHome }) => {
 
       <section className="about-tpl-ds">
         {/* TPL - Bhavani */}
-        <Card
-          style={{ width: 250 }}
-          cover={
-            <img
-              alt="Bhavani Rajan"
-              src="https://i.imgur.com/9TcokzL.png"
-              onMouseOver={e =>
-                (e.currentTarget.src = "https://i.imgur.com/fA047dl.png")
-              }
-              onMouseOut={e => {
-                e.currentTarget.src = "https://i.imgur.com/9TcokzL.png";
-              }}
-            />
-          }
-          actions={[
-            <a
-              href="https://github.com/Bhavani-Rajan"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GithubOutlined key="github" />
-            </a>,
-            <a
-              href="mailto:bhava.rajan.6@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <MailOutlined key="email" />
-            </a>,
-            <a
-              href="https://www.linkedin.com/in/bhavani-rajan/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedinOutlined key="linkedin" />
-            </a>
-          ]}
-        >
-          <Meta
-            avatar={<Avatar src={projectLead} />}
-            title="Bhavani Rajan"
-            description="Team Project Lead"
-          />
-
-          {/* Modal functionality */}
-          <Button
-            className="modal-button"
-            type="primary"
-            onClick={() => setBhavaniModalVisibility(true)}
-          >
-            About me
-          </Button>
-          <Modal
-            id="inside-modal"
-            title="About me"
-            centered
-            visible={bhavaniModalVisibility}
-            onOk={() => setBhavaniModalVisibility(false)}
-            onCancel={() => setBhavaniModalVisibility(false)}
-          >
-            <p>
-              Data Scientist with a background is as a Java Programmer with 3
-              years of corporate experience. I studied at Lambda School,
-              currently working as a Technical Project Lead at Lambda School to
-              support the learning and professional growth of other Lambda
-              students while deepening my skills in Data Science. My skill set
-              includes Data Analysis, Machine Learning Engineering, Predictive
-              Analysis, Recommendation Systems, and Data Engineering. I'm
-              excited to take on a new challenge in the Data Science universe.
-            </p>
-            <Divider>Technical Skills</Divider>
-            <p>
-              Programming Expertise: ​Python (NumPy, Pandas,
-              Scikit-learn,Matplotlib, Seaborn), R, Java, SQL, PostgreSQL,
-              MongoDb, AS/400, DB2/400 and Excel
-            </p>
-            <p>Frameworks: ​TensorFlow, Keras, Flask, Fast API</p>
-            <p>
-              Skills​: Data analysis, Predictive analytics, Linear and
-              multivariate regressions, K-cluster analysis, Classification,
-              Machine learning products, Natural language processing, Neural
-              Networks, Github, Amazon EBS, Heroku
-            </p>
-            <Divider>Hobbies</Divider>
-            <p>Painting and gardening</p>
-            <img
-              alt="Bhavani's paintings"
-              src="https://i.imgur.com/C9JQEKZ.png"
-            />
-            <Divider>Contact</Divider>
-            <div
-              className="inner-buttons"
-              style={{ display: "flex", justifyContent: "space-evenly" }}
-            >
-              <Button
-                href="https://github.com/Bhavani-Rajan"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </Button>
-              <Button
-                href="mailto:bhava.rajan.6@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Email
-              </Button>
-              <Button
-                href="https://www.linkedin.com/in/bhavani-rajan/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LinkedIn
-              </Button>
-            </div>
-          </Modal>
-        </Card>
+        <ProfileCard />
 
         {/* Data Science - Ekram, Zack */}
 

5bea89dbc1a570ecae8d70d176d6c010ea887855
Rachele Edwards <dot.com1472@gmail.com>
2020-10-21 10:09:12 -0600

    render home page test: removed unused imports

--- src/__tests__/RenderHomePage.test.js
+++ src/__tests__/RenderHomePage.test.js
@@ -2,7 +2,6 @@ import RenderHomePage from "../components/pages/Home/RenderHomePage";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 import { render, fireEvent } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import { toggleDrawer } from "../state/actions";

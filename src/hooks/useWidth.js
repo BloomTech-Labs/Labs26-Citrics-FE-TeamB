f17fa6b63d28ea5745d61c09ecc737ea163d285e
David Horstman <davidzilla27@gmail.com>
2020-10-21 14:54:01 -0700

    Allow useWidth to take an optional callback and add comments

--- src/hooks/useWidth.js
+++ src/hooks/useWidth.js
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react";
 
-const getWidth = () =>
-  window.innerWidth ??
-  document.documentElement.clientWidth ??
-  document.body.clientWidth;
-export default function useWidth() {
+/** Hook to track current window width with added de-bouncing logic.
+ * The width will only be updated after resizing has been stopped for 100ms to avoid rapid changes to its value.
+ *
+ * @param {function} [callback] (Optional) Function to execute whenever width changes.
+ * The current width will be passed to this function as its first argument.
+ * @returns The debounced width value.
+ */
+export default function useWidth(callback = null) {
+  // Retrieve the current width of the window
+  const getWidth = () =>
+    window.innerWidth ??
+    document.documentElement.clientWidth ??
+    document.body.clientWidth;
+
   const [width, setWidth] = useState(getWidth());
 
   useEffect(() => {
@@ -13,11 +22,15 @@ export default function useWidth() {
     function handleResize() {
       // prevent execution of previous setTimeout
       clearTimeout(timeoutId);
-      // change width from the state object after 500 milliseconds
-      timeoutId = setTimeout(() => setWidth(getWidth()), 100);
+      // change width from the state object after 100 milliseconds
+      timeoutId = setTimeout(() => {
+        const w = getWidth();
+        setWidth(w);
+        callback && callback(w);
+      }, 100);
     }
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, [width]);
+  }, [callback]);
   return width;
 }
